50f55298ac4bbe0701aa8b2f87a29407078f3262
unknown <devnull@localhost>
2003-12-29 14:51:48 +0000

    removing from trunk

--- sandbox/rdfnet/ResultsFormatMap.py
+++
@@ -1,6 +0,0 @@
-#$Id: ResultsFormatMap.py,v 1.1 2003/10/08 00:55:40 kendall Exp $
-
-formatMap = {
-    "http://www.w3.org/1999/02/22-rdf-syntax-ns#": "rdf-xml",
-    "http://www.w3.org/TR/rdf-testcases/#ntriples": "ntriples"
-    }
