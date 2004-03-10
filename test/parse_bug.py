5a600c85865f7b99cd11ff5b95106cfced99ad96
unknown <devnull@localhost>
2004-03-10 03:30:08 +0000

    cleaned up

--- test/parse_bug.py
+++ test/parse_bug.py
@@ -1,16 +1,13 @@
-from StringIO import StringIO
-from rdflib.InMemoryTripleStore import InMemoryTripleStore
+from rdflib.TripleStore import TripleStore
 from rdflib.Literal import Literal
 from rdflib.URIRef import URIRef
 
-def toString(store):
-    s = StringIO()
-    store.output(s)
-    return s.getvalue()
 
-
-ts = InMemoryTripleStore()
-ts.add(URIRef("http://foo/"), URIRef("http://someslotsite/"), Literal("foo") )
-text = toString(ts)
+ts = TripleStore()
+ts.add((URIRef("http://foo/"), URIRef("http://someslotsite/"), Literal("foo")))
+test = ts.serialize()
 print text
-ts.parse(StringIO(text))
+ts.parse(text)
+
+
+
