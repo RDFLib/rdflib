b3ea49788599305a4047ea5a4f114041eb6c9727
unknown <devnull@localhost>
2004-03-12 22:17:33 +0000

    factored out create_parser into function

--- rdflib/syntax/parsers/RDFXMLParser.py
+++ rdflib/syntax/parsers/RDFXMLParser.py
@@ -5,18 +5,29 @@ from xml.sax.handler import ErrorHandler
 from rdflib.syntax.parsers.RDFXMLHandler import RDFXMLHandler
 
 
+def create_parser(store):
+    parser = make_parser()
+    # Workaround for bug in expatreader.py. Needed when
+    # expatreader is trying to guess a prefix.
+    parser.start_namespace_decl("xml", "http://www.w3.org/XML/1998/namespace")
+    parser.setFeature(handler.feature_namespaces, 1)
+    rdfxml = RDFXMLHandler(store)
+    #rdfxml.setDocumentLocator(_Locator(self.url, self.parser))
+    parser.setContentHandler(rdfxml)
+    parser.setErrorHandler(ErrorHandler())
+    return parser
+
+
 class RDFXMLParser(object):
     short_name = "xml"
 
     def __init__(self, store):
         self.store = store
+        self._parser = create_parser(store)
     
     def parse(self, source):
-        parser = make_parser()
-        # Workaround for bug in expatreader.py. Needed when
-        # expatreader is trying to guess a prefix.
-        parser.start_namespace_decl("xml", "http://www.w3.org/XML/1998/namespace")
-        parser.setFeature(handler.feature_namespaces, 1)
-        parser.setContentHandler(RDFXMLHandler(self.store))
-        parser.setErrorHandler(ErrorHandler())
-        parser.parse(source)
+        #@@ reset self._parser?
+        self._parser.parse(source)
+
+
+
