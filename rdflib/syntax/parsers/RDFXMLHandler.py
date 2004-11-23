c00f04319d53d5f754f0e51a3f16bb848f5d32a9
eikeon <devnull@localhost>
2004-11-23 21:07:01 +0000

    fixed parser bug in the case where there was a parseType of Literal followed by a parseType of Collection

--- rdflib/syntax/parsers/RDFXMLHandler.py
+++ rdflib/syntax/parsers/RDFXMLHandler.py
@@ -391,6 +391,7 @@ class RDFXMLHandler(handler.ContentHandler):
                     next.start = self.property_element_start
                     next.end = self.property_element_end
                 elif parse_type=="Collection":
+                    current.char = None                    
                     next.start = self.node_element_start
                     next.end = self.list_node_element_end
                 else: #if parse_type=="Literal":
