4ee1c5081a7f35a709616f32cea24d8cd2bcee60
eikeon <devnull@localhost>
2005-03-18 05:57:57 +0000

    updated version / dates

--- rdflib/Namespace.py
+++ rdflib/Namespace.py
@@ -7,19 +7,3 @@ class Namespace(URIRef):
         return URIRef(self + key)
 
 
-# TODO: how on earth did these get here?
-def check_subject(s):
-    if not (isinstance(s, URIRef) or isinstance(s, BNode)):
-        raise SubjectTypeError(s)
-
-def check_predicate(p):
-    if not isinstance(p, URIRef):
-        raise PredicateTypeError(p)
-
-def check_object(o):
-    if not (isinstance(o, URIRef) or \
-       isinstance(o, Literal) or \
-       isinstance(o, BNode)):
-        raise ObjectTypeError(o)
-
-
