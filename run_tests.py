2f78b2106a22b8ce93593bed9f55df7acca9db6f
michel <devnull@localhost>
2005-03-29 20:10:33 +0000

    added comments for zodb tests

--- run_tests.py
+++ run_tests.py
@@ -5,6 +5,8 @@ from test.triple_store import *
 from test.type_check import *
 from test.graph import *
 
+# you must export your PYTHONPATH to point to a Z3 installation to get this to work!, like:
+# export PYTHONPATH="export PYTHONPATH="/home/michel/dev/Zope3Trunk/src"
 try:
     import persistent
     from test.zodb import *
