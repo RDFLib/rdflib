4a226efd7d06be559813ad24c3ba253a7182e402
eikeon <devnull@localhost>
2005-04-28 00:19:32 +0000

    applied patch from Stefan

--- rdflib/backends/IOMemory.py
+++ rdflib/backends/IOMemory.py
@@ -10,8 +10,9 @@ class IOMemory(Backend):
     An integer-key-optimized-context-aware-in-memory backend.
 
     Uses nested dictionaries to store triples and context. Each triple
-    is stored in three such indices as follows cspo[c][s][p][o] = 1
-    and cpos[c][p][o][s] = 1 and cosp[c][o][s][p] = 1.
+    is stored in six such indices as follows cspo[c][s][p][o] = 1
+    and cpos[c][p][o][s] = 1 and cosp[c][o][s][p] = 1 as well as
+    spo[s][p][o] = [c] and pos[p][o][s] = [c] and pos[o][s][p] = [c]
 
     Context information is used to track the 'source' of the triple
     data for merging, unmerging, remerging purposes.  context aware
@@ -44,9 +45,7 @@ class IOMemory(Backend):
 
         self.__namespace = self.createPrefixMap()
         self.__prefix = self.createPrefixMap()
-
-        self.count = 0
-
+        
     def bind(self, prefix, namespace):
         self.__prefix[namespace] = prefix
         self.__namespace[prefix] = namespace
@@ -82,15 +81,27 @@ class IOMemory(Backend):
         return (self.reverse[s], self.reverse[p], self.reverse[o])
 
     def uniqueSubjects(self, context=None):
-        for si in self.cspo[context].keys():
+        if context is None:
+            index = sel.spo
+        else:
+            index = self.cspo[context]
+        for si in index.keys():
             yield self.forward[si]
 
     def uniquePredicates(self, context=None):
-        for pi in self.cpos[context].keys():
+        if context is None:
+            index = sel.pos
+        else:
+            index = self.cpos[context]
+        for pi in index.keys():
             yield self.forward[pi]
-
+        
     def uniqueObjects(self, context=None):
-        for oi in self.cosp[context].keys():
+        if context is None:
+            index = sel.osp
+        else:
+            index = self.cosp[context]
+        for oi in index.keys():
             yield self.forward[oi]
 
     def createForward(self):
@@ -113,8 +124,7 @@ class IOMemory(Backend):
         for triple in self.triples(triple, context):
             #triple is already in the store.            
             return
-
-        self.count = self.count + 1
+            
         subject, predicate, object = triple
         context = context or self.default_context
         
@@ -162,100 +172,104 @@ class IOMemory(Backend):
         # add dictionary entries for cspo[c][s][p][o] = 1,
         # cpos[c][p][o][s] = 1, and cosp[c][o][s][p] = 1, creating the
         # nested {} where they do not yet exits.
-
-        # assign cspo[c][s][p][o] = 1
-
-        if self.cspo.has_key(ci):
-            spo = self.cspo[ci]
-        else:
-            spo = self.cspo[ci] = self.createIndex()
-        if spo.has_key(si):
-            po = spo[si]
-        else:
-            po = spo[si] = self.createIndex()
-        if po.has_key(pi):
-            o = po[pi]
-        else:
-            o = po[pi] = self.createIndex()
-        o[oi] = 1
-
-        # cpos[c][p][o][s] = 1
-
-        if self.cpos.has_key(ci):
-            pos = self.cpos[ci]
-        else:
-            pos = self.cpos[ci] = self.createIndex()
-        if pos.has_key(pi):
-            os = pos[pi]
-        else:
-            os = pos[pi] = self.createIndex()
-        if os.has_key(oi):
-            s = os[oi]
-        else:
-            s = os[oi] = self.createIndex()
-        s[si] = 1
-
-        # cosp[c][o][s][p] = 1
-
-        if self.cosp.has_key(ci):
-            osp = self.cosp[ci]
-        else:
-            osp = self.cosp[ci] = self.createIndex()
-        if osp.has_key(oi):
-            sp = osp[oi]
-        else:
-            sp = osp[oi] = self.createIndex()
-        if sp.has_key(si):
-            p = sp[si]
-        else:
-            p = sp[si] = self.createIndex()
-        p[pi] = 1
-
-
+        self._setNestedIndex(self.cspo, ci, si, pi, oi)
+        self._setNestedIndex(self.cpos, ci, pi, oi, si)
+        self._setNestedIndex(self.cosp, ci, oi, si, pi)
+        
+        self._setNestedIndex(self.spo, si, pi, oi, ci)
+        self._setNestedIndex(self.pos, pi, oi, si, ci)
+        self._setNestedIndex(self.osp, oi, si, pi, ci)
+        
+    def _setNestedIndex(self, index, *keys):
+        for key in keys[:-1]:
+            if not index.has_key(key):
+                index[key] = self.createIndex()
+            index = index[key]
+        index[keys[-1]] = 1
+        
+    
+    def _removeNestedIndex(self, index, *keys):
+        """ Remove context from the list of contexts in a nested index.
+        
+        Afterwards, recursively remove nested indexes when they became empty.
+        """
+        parents = []
+        for key in keys[:-1]:
+            parents.append(index)    
+            index = index[key]
+        del index[keys[-1]]
+        
+        n = len(parents)
+        for i in xrange(n):
+            index = parents[n-1-i]
+            key = keys[n-1-i]
+            if len(index[key]) == 0:
+                del index[key]
+        
     def remove_context(self, context):
-        self.remove((Any, Any, Any, context))
+        self.remove((Any, Any, Any), context)
+        try:
+            ci = self.reverse[context]
+            del self.cspo[ci], self.cpos[ci], self.cosp[ci]
+        except KeyError:
+            # TODO: no exception when removing non-existant context?
+            pass
 
     def remove(self, triple, context=None):
         f = self.forward
         r = self.reverse
         if context is None:
-            # TODO: this needs to be replaced with something more efficient
-            for context in self.contexts():
-                for triple in self.triples(triple, context):
-                    try:
-                        subject, predicate, object = triple        
-                        si, pi, oi = self.identifierToInt((subject, predicate, object))
-                        ci = r[context]
-                        del self.cspo[ci][si][pi][oi]
-                        del self.cpos[ci][pi][oi][si]
-                        del self.cosp[ci][oi][si][pi]
-                        self.count = self.count - 1
-                    except KeyError:
-                        continue
-
-            # grr!! hafta ref-count these before you can collect them dumbass!
-#             del f[si]
-#             del f[pi]
-#             del f[oi]
-#             del r[subject]
-#             del r[predicate]
-#             del r[object]
+            for triple in self.triples(triple):
+                subject, predicate, object = triple        
+                si, pi, oi = self.identifierToInt((subject, predicate, object))
+                contexts = list(self.contexts(triple))
+                for context in contexts:
+                    ci = r[context]
+                    del self.cspo[ci][si][pi][oi]
+                    del self.cpos[ci][pi][oi][si]
+                    del self.cosp[ci][oi][si][pi]
+                    
+                    self._removeNestedIndex(self.spo, si, pi, oi, ci)
+                    self._removeNestedIndex(self.pos, pi, oi, si, ci)
+                    self._removeNestedIndex(self.osp, oi, si, pi, ci)
+                    # grr!! hafta ref-count these before you can collect them dumbass!
+                    #del f[si], f[pi], f[oi]
+                    #del r[subject], r[predicate], r[object]
+        else:
+            ci = r[context]
+            for triple in self.triples(triple, context):
+                subject, predicate, object = triple
+                si, pi, oi = self.identifierToInt((subject, predicate, object))    
+                del self.cspo[ci][si][pi][oi]
+                del self.cpos[ci][pi][oi][si]
+                del self.cosp[ci][oi][si][pi]
+                
+                self._removeNestedIndex(self.spo, si, pi, oi, ci)
+                self._removeNestedIndex(self.pos, pi, oi, si, ci)
+                self._removeNestedIndex(self.osp, oi, si, pi, ci)
+                # TODO delete references to resources in self.forward/self.reverse 
+                # that are not in use anymore...
 
 
 
     def triples(self, triple, context=None):
         """A generator over all the triples matching """
         subject, predicate, object = triple
-        if context is None:
-            # TODO: this needs to be replaced with something more efficient
-            for c in self.contexts():
-                for triple in self.triples(triple, c):
-                    yield triple
-            return
-
         ci = si = pi = oi = Any
+        
+        if context is None:
+            spo = self.spo
+            pos = self.pos
+            osp = self.osp
+        else:
+            try:
+                ci = self.reverse[context]  # TODO: Really ignore keyerror here
+                spo = self.cspo[ci]
+                pos = self.cpos[ci]
+                osp = self.cosp[ci]
+            except KeyError:
+                return
         try:
-            ci = self.reverse[context]  # throws a keyerror if not context
             if subject is not Any:
                 si = self.reverse[subject] # throws keyerror if subject doesn't exist ;(
             if predicate is not Any:
@@ -266,7 +280,6 @@ class IOMemory(Backend):
             return #raise StopIteration
 
         if si != Any: # subject is given
-            spo = self.cspo[ci]
             if spo.has_key(si):
                 subjectDictionary = spo[si]
                 if pi != Any: # subject+predicate is given
@@ -298,7 +311,6 @@ class IOMemory(Backend):
             else: # given subject not found
                 pass
         elif pi != Any: # predicate is given, subject unbound
-            pos = self.cpos[ci]
             if pos.has_key(pi):
                 predicateDictionary = pos[pi]
                 if oi != Any: # predicate+object is given, subject unbound
@@ -314,7 +326,6 @@ class IOMemory(Backend):
                             ss, pp, oo = self.intToIdentifier((s, pi, o))
                             yield (ss, pp, oo)
         elif oi != Any: # object is given, subject+predicate unbound
-            osp = self.cosp[ci]
             if osp.has_key(oi):
                 objectDictionary = osp[oi]
                 for s in objectDictionary.keys():
@@ -322,7 +333,6 @@ class IOMemory(Backend):
                         ss, pp, oo = self.intToIdentifier((s, p, oi))
                         yield (ss, pp, oo)
         else: # subject+predicate+object unbound
-            spo = self.cspo[ci]
             for s in spo.keys():
                 subjectDictionary = spo[s]
                 for p in subjectDictionary.keys():
@@ -331,19 +341,20 @@ class IOMemory(Backend):
                         yield (ss, pp, oo)
 
     def __len__(self, context):
-        if context is None:
-            return self.count
-        else:
-            # TODO: for eff. implementation
-            count = 0
-            for triple in self.triples((None, None, None), context):
-                count += 1
-            return count
-        
+        # TODO: for eff. implementation
+        count = 0
+        for triple in self.triples((Any, Any, Any), context):
+            count += 1
+        return count
+    
     def contexts(self, triple=None):
-        assert triple is None, "TODO: NYI"
-        for ci in self.cspo.keys():
-            yield self.forward[ci]
+        if triple:
+            si, pi, oi = self.identifierToInt(triple)
+            for ci in self.spo[si][pi][oi]:
+                yield self.forward[ci]
+        else:
+            for ci in self.cspo.keys():
+                yield self.forward[ci]
 
 
 
