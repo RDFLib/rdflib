e2a843519c56fa3457d3cdcb77b5a9bfa6bb37d3
eikeon <devnull@localhost>
2005-05-01 17:53:57 +0000

    added check_ methods

--- rdflib/InformationStore.py
+++ rdflib/InformationStore.py
@@ -3,15 +3,17 @@
 from rdflib import Graph
 from rdflib.Namespace import Namespace
 
+from rdflib.util import check_subject, check_predicate, check_object, check_context
+
 INFORMATION_STORE = Namespace("http://rdflib.net/2002/InformationStore#")
 CONTEXT = INFORMATION_STORE["Context"]
 SOURCE = INFORMATION_STORE["source"]
+
 
 class InformationStore(Graph):
     """
     Depcrecated. Use Graph instead.
     """
-
     
     def __init__(self, path=None, backend=None):
         if backend==None:
