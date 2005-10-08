851679a8b05ecea8ac28603a840577499c6ba43b
eikeon <devnull@localhost>
2005-10-08 07:38:32 +0000

    removed tab

--- test/nt.py
+++ test/nt.py
@@ -13,6 +13,7 @@ class NTTestCase(unittest.TestCase):
     def testModel(self):
 	g = Graph()
         g.load("http://www.w3.org/2000/10/rdf-tests/rdfcore/rdfms-empty-property-elements/test002.nt", format="nt")
+        g.load("http://www.w3.org/2000/10/rdf-tests/rdfcore/rdfms-empty-property-elements/test002.nt", format="nt")
 
 
 if __name__ == "__main__":
