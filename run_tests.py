0508ada208ac4a50c320576d5bd4309c70b364ea
eikeon <devnull@localhost>
2005-12-07 06:18:00 +0000

    updated to include all but test.type_check

--- run_tests.py
+++ run_tests.py
@@ -1,26 +1,29 @@
 import unittest
 
+from test.identifier_equality import *
 
 from test.store import *
+from test.graph import *
+from test.triple_store import *
 from test.context import *
 
-#from test.util import *
-#from test.n3 import *
-#from test.seq import SeqTestCase
+# Graph no longer has the type checking at the moment. Do we want to
+# put it back? Should we?
+#
+# from test.type_check import * 
 
-#from test.identifier_equality import *
-#from test.triple_store import *
-#from test.type_check import *
-#from test.graph import *
-#from test.rdf import *
+from test.parser import *
+from test.parser_rdfcore import *
 
-#from test.parser import *
-#from test.parser_rdfcore import *
+from test.rdf import * # how does this manage to be 9 tests?
 
+from test.n3 import *
+from test.nt import *
 
-#from test.nt import *
-#from test.rules import PychinkoTestCase
+from test.util import *
+from test.seq import SeqTestCase
 
+from test.rules import *
 
 
 if __name__ == "__main__":
