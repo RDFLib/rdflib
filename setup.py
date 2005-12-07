b0c8cb83fa0e573a8c08520a48d8252eaba100a9
eikeon <devnull@localhost>
2005-12-07 06:24:12 +0000

    synced with changes to setup.py in 2.2.x

--- setup.py
+++ setup.py
@@ -1,31 +1,37 @@
 # Install rdflib
 from distutils.core import setup
-from rdflib import __version__, __date__
+from rdflib import __version__
 
-from distutils.command.install import install as _install
+from distutils.core import Command
 
-def rename_old():
-    "Look for and rename rdflib if one has already been installed."
-    from distutils.sysconfig import get_python_lib
-    from os import rename
-    from os.path import join, exists
-    from time import time
-    
-    lib_dir = get_python_lib()
-    rdflib_dir = join(lib_dir, "rdflib")
-    if exists(rdflib_dir):
-        backup = "%s-%s" % (rdflib_dir, int(time()))    
-        print "Renaming previously installed rdflib to: \n  %s" % backup
-        rename(rdflib_dir, backup)
 
-class install(_install):
+class uninstall(Command):
+    description = "uninstall everything by renaming rdflib to 'rdflib-%s' % int(time())"
+
+    user_options = [ ]
+
+    def initialize_options (self):
+	pass
+
+    def finalize_options (self):
+	pass
+
     def run (self):
-        rename_old()
-        _install.run(self)
+	"Look for and rename rdflib if one has already been installed."
+	from distutils.sysconfig import get_python_lib
+	from os import rename
+	from os.path import join, exists
+	from time import time
 
+	lib_dir = get_python_lib()
+	rdflib_dir = join(lib_dir, "rdflib")
+	if exists(rdflib_dir):
+	    backup = "%s-%s" % (rdflib_dir, int(time()))    
+	    print "Renaming previously installed rdflib to: \n  %s" % backup
+	    rename(rdflib_dir, backup)
 
 setup(
-    cmdclass={'install': install},
+    cmdclass={'uninstall': uninstall},
     name = 'rdflib',
     version = __version__,
     description = "RDF library containing an RDF triple store and RDF/XML parser/serializer",
@@ -38,7 +44,7 @@ setup(
     platforms = ["any"],
     classifiers = ["Programming Language :: Python"],
     long_description = "RDF library containing an RDF triple store and RDF/XML parser/serializer",
-    dowload_url = "http://rdflib.net/%s/rdflib-%s.tar.gz" % (__date__, __version__),
+    download_url = "http://rdflib.net/%s/rdflib-%s.tar.gz" % (__date__, __version__),
 
     packages = ['rdflib',
                 'rdflib.backends',
@@ -48,5 +54,4 @@ setup(
                 'rdflib.syntax.parsers',
                 'rdflib.syntax.parsers.n3p'],    
     )
-
 
