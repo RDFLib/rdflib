66d1c8c86d6b15f46e4e5fe385a4c59fe70cd3df
eikeon <devnull@localhost>
2005-12-07 00:15:37 +0000

    checking in with tests commented out to test new release script

--- run_tests.py
+++ run_tests.py
@@ -1,22 +1,19 @@
 import unittest
 
-from test.rules import PychinkoTestCase
+#from test.rules import PychinkoTestCase
+#from test.util import *
+#from test.n3 import *
+#from test.seq import SeqTestCase
+#from test.identifier_equality import *
+#from test.triple_store import *
+#from test.type_check import *
 #from test.graph import *
-
-if False:
-    from test.n3 import *
-    from test.seq import SeqTestCase
-    from test.identifier_equality import *
-    from test.triple_store import *
-    #from test.type_check import *
-    from test.graph import *
-    from test.rdf import *
-    #from test.store import *
-    from test.parser import *
-    #from test.context import *
-    #from test.nt import *
-
-    #from test.parser_rdfcore import *
+#from test.rdf import *
+#from test.store import *
+#from test.parser import *
+#from test.context import *
+#from test.nt import *
+#from test.parser_rdfcore import *
 
 
 if __name__ == "__main__":
