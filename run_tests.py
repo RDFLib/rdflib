cf5830e8905c0db0356b4519bd93a1de3fe5545a
eikeon <devnull@localhost>
2005-12-07 03:26:46 +0000

    commented back in a few tests

--- run_tests.py
+++ run_tests.py
@@ -1,15 +1,15 @@
 import unittest
 
 #from test.rules import PychinkoTestCase
-#from test.util import *
-#from test.n3 import *
+from test.util import *
+from test.n3 import *
 #from test.seq import SeqTestCase
 #from test.identifier_equality import *
 #from test.triple_store import *
 #from test.type_check import *
 #from test.graph import *
 #from test.rdf import *
-#from test.store import *
+from test.store import *
 #from test.parser import *
 #from test.context import *
 #from test.nt import *
