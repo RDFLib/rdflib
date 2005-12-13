ab7ad04bb674dec9464f84326e6d82cf2e527a0b
chimezie <devnull@localhost>
2005-12-13 02:52:49 +0000

    moved some documentation from http://rdflib/rdflib_store into stub

--- rdflib/store/__init__.py
+++ rdflib/store/__init__.py
@@ -1,7 +1,21 @@
+## Context-aware: An RDF store capable of storing statements within contexts is considered context-aware.
+## Essentially, such a store is able to partition the RDF model it represents into individual, named, and addressable sub-graphs.
 
+## Formula-aware: An RDF store capable of distinguishing between statements that are asserted and statements
+## that are quoted is considered formula-aware.
+
+## Conjunctive Graph: This refers to the 'top-level' Graph. It is the aggregation of all the contexts
+## within it and is also the appropriate, absolute boundary for closed world assumptions / models.
+
+## For the sake of persistence, Conjunctive Graphs must be distinguished by identifiers (that may not
+## neccessarily be RDF identifiers or may be an RDF identifier normalized - SHA1/MD5 perhaps - for database
+## naming purposes ).
+
+## Conjunctive Query: Any query that doesn't limit the store to search within a named context only. Such a
+## query expects a context-aware store to search the entire asserted universe (the conjunctive graph).
+## A formula-aware store is expected not to include quoted statements when matching such a query.
 
 class Store(object):
-
     #Properties
     context_aware = False
     formula_aware = False
@@ -27,34 +41,55 @@ class Store(object):
         """
 
     def close(self, commit_pending_transaction=False):
-        """ """
+        """
+        This closes the database connection. The commit_pending_transaction parameter specifies whether to
+        commit all pending transactions before closing (if the store is transactional).        
+        """
 
     def destroy(self, configuration):
-        """ """        
+        """
+        This destroys the instance of the store identified by the configuration string.        
+        """        
 
     def add(self, (subject, predicate, object), context, quoted=False):
-        """ Add a triple to the store of triples. """
+        """
+        Adds the given statement to a specific context or to the model. The quoted argument
+        is interpreted by formula-aware stores to indicate this statement is quoted/hypothetical
+        It should be an error to not specify a context and have the quoted argument be True.
+        It should also be an error for the quoted argument to be True when the store is not formula-aware.        
+        """
 
     def remove(self, (subject, predicate, object), context=None):
-        """ Remove a triple from the store """
+        """ Remove the set of triples matching the pattern from the store """
 
     def triples(self, (subject, predicate, object), context=None):
         """ 
         A generator over all the triples matching pattern. Pattern can
         be any objects for comparing against nodes in the store, for
         example, RegExLiteral, Date? DateRange?
+
+        A conjunctive query can be indicated by either providing a value of None
+        for the context or the identifier associated with the Conjunctive Graph (if it's context aware).
         """
 
     # varients of triples will be done if / when optimization is needed
 
     def __len__(self, context=None):
-        """ Number of statements in the store. """
+        """
+        Number of statements in the store. This should only account for non-quoted (asserted) statements
+        if the context is not specified, otherwise it should return the number of statements in the formula or context given.
+        """
 
     def contexts(self, triple=None):
-        """ """
+        """
+        Generator over all contexts in the graph. If triple is specified, a generator over all
+        contexts the triple is in.
+        """
 
     def remove_context(self, identifier):
-        """ """        
+        """
+        Removes the given context from the graph.
+        """        
 
     # Optional Namespace methods
 
