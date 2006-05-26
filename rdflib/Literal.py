9336791ed8b435b93cb35b19b46b9505a24afe50
chimezie <devnull@localhost>
2006-05-26 03:57:22 +0000

    added support for Literal casting to and from python datatypes (in the constructor and in a toPython function)

--- rdflib/Literal.py
+++ rdflib/Literal.py
@@ -150,7 +150,7 @@ class Literal(Identifier):
         if klass:
             return "%s(%s)"%(klass.__name__,str(self))
         else:
-            return """<Literal language=%s datatype=%s value=%s>""" % (repr(self.language), repr(self.datatype), unicode(self))
+            return """rdflib.Literal(%s,language=%s,datatype=%s>""" % (unicode(self),repr(self.language), repr(self.datatype))
 
     def toPython(self):
         """
