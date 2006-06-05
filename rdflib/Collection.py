fbbd9c7a14e21dede6ebbf19e56a5964212b6918
eikeon <devnull@localhost>
2006-06-05 17:13:52 +0000

    minor cleanup

--- rdflib/Collection.py
+++ rdflib/Collection.py
@@ -1,8 +1,9 @@
+
 class Collection(object):
     """See 3.3.5 Emulating container types: http://docs.python.org/ref/sequence-types.html#l2h-232"""
     
-    def __init__(self, g, uri, seq=[]):
-        self.g = g
+    def __init__(self, graph, uri, seq=[]):
+        self.graph = graph
         self.uri = uri or BNode()
         for item in seq:
             self.append(item)
@@ -10,11 +11,12 @@ class Collection(object):
     def _get_container(self, index):
         """Gets the first, rest holding node at index."""
         assert isinstance(index, int)
+        graph = self.graph
         container = self.uri
         i = 0
         while i<index: 
             i += 1
-            container = g.value(container, RDF.rest)
+            container = graph.value(container, RDF.rest)
             if container is None:
                 break
         return container
@@ -22,16 +24,15 @@ class Collection(object):
     def __len__(self):
         """length of items in collection."""
         count = 0
-        for item in g.items(self.uri):
+        for item in self.graph.items(self.uri):
             count += 1
         return count
 
     def __getitem__(self, key):
-        """..."""
-        #print "Get item %s list." % key
+        """TODO"""
         c = self._get_container(key)
         if c:
-            v = g.value(c, RDF.first)
+            v = self.graph.value(c, RDF.first)
             if v:
                 return v
             else:
@@ -40,48 +41,48 @@ class Collection(object):
             raise IndexError, key
 
     def __setitem__(self, key, value):
-        """..."""
-        #print "Set item %s to %s list." % (key, value)
+        """TODO"""
         c = self._get_container(key)
         if c:
-            g.add((c, RDF.first, value))
+            self.graph.add((c, RDF.first, value))
         else:
             raise IndexError, key
         
 
     def __delitem__(self, key):
         """..."""
-        #print "Delete item %s list." % key
         self[key] # to raise any potential key exceptions
+        graph = self.graph
         current = self._get_container(key)
         assert current
         next = self._get_container(key+1)
         # TODO: delete last element in list
         assert next
-        first = g.value(next, RDF.first)
-        rest = g.value(next, RDF.rest)
+        first = graph.value(next, RDF.first)
+        rest = graph.value(next, RDF.rest)
 
-        g.set((current, RDF.first, first))
-        g.set((current, RDF.rest, rest))
+        graph.set((current, RDF.first, first))
+        graph.set((current, RDF.rest, rest))
 
     def __iter__(self):
         """Iterator over items in Collections"""
-        return g.items(self.uri)
+        return self.graph.items(self.uri)
         
     def append(self, item):
         container = self.uri
+        graph = self.graph
         while True:
-            first = g.value(container, RDF.first)
+            first = graph.value(container, RDF.first)
             if first is None:
-                g.add((container, RDF.first, item))
+                graph.add((container, RDF.first, item))
                 return
             else:
-                rest = g.value(container, RDF.rest)
+                rest = graph.value(container, RDF.rest)
                 if rest:
                     container = rest
                 else:
                     node = BNode()
-                    g.add((container, RDF.rest, node))
+                    graph.add((container, RDF.rest, node))
                     container = node
 
 
