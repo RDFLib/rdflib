0790e9e509070af4b5ee2e3fac87264c2c23a5c3
ogrisel <devnull@localhost>
2006-06-25 15:43:35 +0000

    trailing spaces

--- rdflib/sparql/bison/__init__.py
+++ rdflib/sparql/bison/__init__.py
@@ -2,14 +2,14 @@ import SPARQLParserc as SPARQLParser
 from SPARQLEvaluate import Evaluate, NotImplemented
 
 def CreateSPARQLParser():
-    return SPARQLParser.new()    
+    return SPARQLParser.new()
 
-def Parse(query,debug = False):    
+def Parse(query,debug = False):
     p = CreateSPARQLParser()
     if debug:
         try:
            p.debug_mode(1)
         except:
-            p.debug = 1    
+            p.debug = 1
     return p.parse(unicode(query,'utf-8'))
 
