11ede169910f70af6b030cbf24be6e040ca1cbfd
chimezie <devnull@localhost>
2006-06-27 15:22:08 +0000

    added test case for default graph

--- test/aggregate_graphs.py
+++ test/aggregate_graphs.py
@@ -51,6 +51,14 @@ PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 SELECT ?class
 WHERE { GRAPH ?graph { ?member a ?class } }"""
 
+sparqlQ3 =\
+"""
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> 
+PREFIX log: <http://www.w3.org/2000/10/swap/log#>
+SELECT ?n3Doc
+WHERE {?n3Doc a log:N3Document }"""
+
+
 def testAggregateRaw():
     memStore = plugin.get('IOMemory',Store)()
     graph1 = Graph(memStore)
@@ -100,6 +108,22 @@ def testAggregateSPARQL():
     #print rt.serialize(format='json')
     assert rt.serialize('python')[0] == LOG_NS.N3Document,str(rt)
 
+def testDefaultGraph():
+    memStore = plugin.get('IOMemory',Store)()
+    graph1 = Graph(memStore,URIRef("graph1"))
+    graph2 = Graph(memStore,URIRef("graph2"))
+    graph3 = Graph(memStore,URIRef("graph3"))
+    
+    for n3Str,graph in [(testGraph1N3,graph1),
+                        (testGraph2N3,graph2),
+                        (testGraph3N3,graph3)]:
+        graph.parse(StringIO(n3Str),format='n3')
+    G = ConjunctiveGraph(memStore)
+    #test that CG includes triples from all 3
+    assert G.query(sparqlQ3),"CG as default graph should *all* triples"
+    assert not graph2.query(sparqlQ3),"Graph as default graph should *not* include triples from other graphs"
+
 if __name__ == '__main__':
     #testAggregateRaw()
-    testAggregateSPARQL()
+    #testAggregateSPARQL()
+    testDefaultGraph()
