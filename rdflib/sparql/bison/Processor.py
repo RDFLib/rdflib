80e2846ead6e0822f6eb79d4d20186af39e01d13
chimezie <devnull@localhost>
2006-06-27 15:20:02 +0000

    CreateSPARQLParser and Parse moved to rdflib.sparql.bisonProcessor.

--- rdflib/sparql/bison/Processor.py
+++ rdflib/sparql/bison/Processor.py
@@ -1,13 +1,24 @@
 from rdflib import sparql
-from rdflib.sparql.bison.Query import Query, Prolog
-from rdflib.sparql.bison import Parse, Evaluate
+from Query import Query, Prolog
+from SPARQLEvaluate import Evaluate        
+import SPARQLParserc as SPARQLParser
 
-#from cStringIO import StringIO
+def CreateSPARQLParser():
+    return SPARQLParser.new()    
+
+def Parse(query,debug = False):    
+    p = CreateSPARQLParser()
+    if debug:
+        try:
+           p.debug_mode(1)
+        except:
+            p.debug = 1    
+    return p.parse(unicode(query,'utf-8'))
 
 class Processor(sparql.Processor):
 
-    def __init__(self, store):
-        self.store = store
+    def __init__(self, graph):
+        self.graph = graph
 
     def query(self, strOrQuery, initBindings={}, initNs={}, DEBUG=False):
         assert isinstance(strOrQuery, (basestring, Query)), "%s must be a string or an rdflib.sparql.bison.Query.Query instance"%strOrQuery
@@ -20,4 +31,4 @@ class Processor(sparql.Processor):
             for prefix, nsInst in initNs.items():
                 if prefix not in strOrQuery.prolog.prefixBindings:
                     strOrQuery.prolog.prefixBindings[prefix] = nsInst
-        return  Evaluate(self.store, strOrQuery, initBindings, DEBUG=DEBUG)
+        return  Evaluate(self.graph, strOrQuery, initBindings, DEBUG=DEBUG)
--- rdflib/sparql/bison/__init__.py
+++ rdflib/sparql/bison/__init__.py
@@ -1,15 +1,1 @@
-import SPARQLParserc as SPARQLParser
-from SPARQLEvaluate import Evaluate, NotImplemented
-
-def CreateSPARQLParser():
-    return SPARQLParser.new()
-
-def Parse(query,debug = False):
-    p = CreateSPARQLParser()
-    if debug:
-        try:
-           p.debug_mode(1)
-        except:
-            p.debug = 1
-    return p.parse(unicode(query,'utf-8'))
-
+from Processor import CreateSPARQLParser,Parse
