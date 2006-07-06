af175344f835f0835d49e7a854f92a8c7aa2f416
chimezie <devnull@localhost>
2006-07-06 17:38:18 +0000

    Added a common RDFTerm super class for collections, BNodes and resources.  Added a default BNode identifier for ParsedCollections, a method for setting property value lists on ParsedCollection instances and propVals attribute for properties on a collection besides the rdf:first/rdf:rest connections

--- rdflib/sparql/bison/Resource.py
+++ rdflib/sparql/bison/Resource.py
@@ -2,7 +2,12 @@ from rdflib import URIRef, BNode
 from Util import ListRedirect
 from sets import Set
 
-class Resource(object):
+class RDFTerm(object):
+    """
+    Common class for RDF terms
+    """
+
+class Resource(RDFTerm):
     """
     Represents a sigle resource in a triple pattern.  It consists of an identifier
     (URIReff or BNode) and a list of rdflib.sparql.bison.Triples.PropertyValue instances
@@ -21,7 +26,7 @@ class Resource(object):
             for obj in objs:
                 yield (self.identifier,prop,obj)
 
-class TwiceReferencedBlankNode(Resource):
+class TwiceReferencedBlankNode(RDFTerm):
     """
     Represents BNode in triple patterns in this form:
     [ :prop1 :val1 ] :prop2 :val2
@@ -30,13 +35,19 @@ class TwiceReferencedBlankNode(Resource):
         self.identifier = BNode()
         self.propVals = list(Set(props1+props2))
 
-class ParsedCollection(ListRedirect):
+class ParsedCollection(ListRedirect,RDFTerm):
     """
     An RDF Collection
     """
     reducable = False
     def __init__(self,graphNodeList):
+        self.identifier = BNode()
+        self.propVals = []
         self._list = graphNodeList
+        
+    def setPropertyValueList(self,propertyValueList):
+        self.propVals = propertyValueList
+        
     def __repr__(self):
         return "<RDF Collection: %s>"%self._list
         
