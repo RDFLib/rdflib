ce2841c4eaa81a749f62b764e7274f3756f13ca4
chimezie <devnull@localhost>
2006-07-15 05:37:29 +0000

    added doc tests and an index function

--- rdflib/Collection.py
+++ rdflib/Collection.py
@@ -1,9 +1,32 @@
-from rdflib import RDF, BNode
-
+from rdflib import RDF, BNode, Literal
+from rdflib.Graph import Graph
 
 class Collection(object):
-    """See 3.3.5 Emulating container types: http://docs.python.org/ref/sequence-types.html#l2h-232"""
-
+    """
+    See 3.3.5 Emulating container types: http://docs.python.org/ref/sequence-types.html#l2h-232
+    
+    >>> listName = BNode()
+    >>> g = Graph('IOMemory')
+    >>> listItem1 = BNode()
+    >>> listItem2 = BNode()
+    >>> g.add((listName,RDF.first,Literal(1)))
+    >>> g.add((listName,RDF.rest,listItem1))
+    >>> g.add((listItem1,RDF.first,Literal(2)))
+    >>> g.add((listItem1,RDF.rest,listItem2))
+    >>> g.add((listItem2,RDF.rest,RDF.nil))
+    >>> g.add((listItem2,RDF.first,Literal(3)))
+    >>> c=Collection(g,listName)
+    >>> print list(c)
+    [long(1), long(2), long(3)]
+    >>> 1 in c
+    True
+    >>> len(c)
+    3
+    >>> c._get_container(1) == listItem1
+    True
+    >>> c.index(Literal(2)) == 1
+    True
+    """
     def __init__(self, graph, uri, seq=[]):
         self.graph = graph
         self.uri = uri or BNode()
@@ -29,6 +52,26 @@ class Collection(object):
         for item in self.graph.items(self.uri):
             count += 1
         return count
+
+    def index(self, item):
+        """
+        Returns the 0-based numerical index of the item in the list          
+        """
+        listName = self.uri
+        index = 0
+        while True:
+            if (listName,RDF.first,item) in self.graph:
+                return index
+            else:
+                newLink = list(self.graph.objects(listName,RDF.rest))
+                index += 1
+                if newLink == [RDF.nil]:
+                    raise ValueError("%s is not in %s"%(item,self.uri))
+                elif not newLink:
+                    raise Exception("Malformed RDF Collection: %s"%self.uri)
+                else:
+                    assert len(newLink)==1, "Malformed RDF Collection: %s"%self.uri
+                    listName = newLink[0]
 
     def __getitem__(self, key):
         """TODO"""
@@ -98,10 +141,12 @@ class Collection(object):
             graph.remove((container, RDF.first, None))
             graph.remove((container, RDF.rest, None))
             container = rest
-
+def test():
+    import doctest
+    doctest.testmod()
 
 if __name__=="__main__":
-    from rdflib import Graph, BNode
+    test()
 
     g = Graph()
 
