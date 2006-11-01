1fdecc85c92f6b55003f2e49ecb8041d3828e59c
eikeon <devnull@localhost>
2006-11-01 04:45:12 +0000

    Unhackified by making it not be a subclass of URIRef (anyone have code that depends on Namespace being a subclass of URIRef?)

--- rdflib/Namespace.py
+++ rdflib/Namespace.py
@@ -1,15 +1,18 @@
 from rdflib.URIRef import URIRef
 
 
-class Namespace(URIRef):
+class Namespace(object):
+
+    def __init__(self, value):
+        self.__value = value
 
     def __getitem__(self, key, default=None):
-        return URIRef(self + key)
+        return URIRef(self.__value + key)
 
     def __getattr__(self, name):
         if name.startswith("__"): # ignore any special Python names!
             raise AttributeError
         else:
-            return URIRef(self + name)
+            return URIRef(self.__value + name)
 
 
