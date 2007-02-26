5b0d6eb4793f7556d20857f649649a9f71904910
gjh <devnull@localhost>
2007-02-26 02:51:38 +0000

    refactoring, removal of redundant imports (courtesy Stefano Debenedetti)

--- test/sparql/testSPARQL.py
+++ test/sparql/testSPARQL.py
@@ -9,16 +9,16 @@
 """
 import sys, os, time, datetime
 
-from rdflib.sparql import ns_rdf  as ns_rdf
-from rdflib.sparql import ns_rdfs as ns_rdfs
-from rdflib.sparql import ns_dc   as ns_dc
-from rdflib.sparql import ns_owl  as ns_owl
+from rdflib.constants   import RDFNS  as ns_rdf
+from rdflib.constants   import RDFSNS as ns_rdfs
+#from rdflib.sparql import ns_dc   as ns_dc
+#from rdflib.sparql import ns_owl  as ns_owl
 
-from rdflib.sparql import type_integer
-from rdflib.sparql import type_double
-from rdflib.sparql import type_float
-from rdflib.sparql import type_decimal
-from rdflib.sparql import type_dateTime
+from rdflib.sparql.sparql import type_integer
+from rdflib.sparql.sparql import type_double
+from rdflib.sparql.sparql import type_float
+from rdflib.sparql.sparql import type_decimal
+from rdflib.sparql.sparql import type_dateTime
 
 from rdflib.Namespace import Namespace
 
@@ -28,5 +28,6 @@ ns_book   = Namespace("http://example.org/book")
 ns_person = Namespace("http://example.org/person#")
 ns_dt     = Namespace("http://example.org/datatype#")
 ns_dc0    = Namespace("http://purl.org/dc/elements/1.0/")
+ns_dc     = Namespace("http://purl.org/dc/elements/1.1/")
 ns_vcard  = Namespace("http://www.w3.org/2001/vcard-rdf/3.0#")
 
