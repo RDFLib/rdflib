d431a7486791c66f9d49724dca90836b720ae98a
eikeon <devnull@localhost>
2007-03-11 23:01:10 +0000

    added raw input for why identifier equality is the way it is

--- test/IdentifierEquality.py
+++ test/IdentifierEquality.py
@@ -7,6 +7,25 @@ from rdflib.syntax.parsers.RDFXMLHandler import CORE_SYNTAX_TERMS
 from rdflib.Graph import Graph
 from rdflib import RDF
 
+"""
+Ah... it's coming back to me...
+[6:32p] eikeon: think it's so transitivity holds...
+[6:32p] eikeon: if a==b and b==c then a should == c
+[6:32p] eikeon: "foo"==Literal("foo")
+[6:33p] eikeon: We don't want URIRef("foo")==Literal("foo")
+[6:33p] eikeon: But if we have URIRef("foo")=="foo" then it implies it.
+[6:33p] chimezie: yes, definately not the other RDFLib 'typed' RDF (and N3) terms
+[6:34p] eikeon: Why do you need URIRef("foo")=="foo" ?
+[6:34p] chimezie: i'm just wondering if a URI and a string with the same lexical value, are by definition 'different'
+[6:35p] eikeon: Think so, actually. Think of trying to serialize some triples.
+[6:36p] eikeon: If they are the same you'd serialize them the same, no?
+[6:36p] chimezie: I guess I was thinking of a 'string' in a native datatype sense, not in the RDF sense (where they would be distinctly different)
+[6:37p] eikeon: We should try and brain dump some of this...
+[6:37p] eikeon: it look a fairly long time to work out.
+[6:37p] eikeon: But think we finally landed in the right spot.
+[6:38p] eikeon: I know many of the backends break if URIRef("foo")==Literal("foo")
+[6:39p] eikeon: And if we want "foo"==Literal("foo") --- then we really can't have URIRef("foo") also == "foo"
+"""
 class IdentifierEquality(unittest.TestCase):
 
     def setUp(self):
