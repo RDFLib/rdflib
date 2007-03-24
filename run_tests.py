fa0c772704cae43e7b8013c66296aab5f49079ab
eikeon <devnull@localhost>
2007-03-24 16:21:30 +0000

    changed UTF-8 -> utf-8

--- run_tests.py
+++ run_tests.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: UTF-8 -*-
+# -*- coding: utf-8 -*-
 """
 This test runner uses Nose for test discovery and running. It uses the argument
 spec of Nose, but with some options pre-set.
