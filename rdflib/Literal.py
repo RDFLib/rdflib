75da77d0bbc7c7fc89b95489c4acd15b41727652
chimezie <devnull@localhost>
2007-06-15 09:53:20 +0000

    fix for test_datatype_parsing.py.  Added __hash__ method which is used (instead of __eq__) internally to manage dictionary key comparison.

--- rdflib/Literal.py
+++ rdflib/Literal.py
@@ -166,6 +166,37 @@ class Literal(Identifier):
         """
         return not self.__eq__(other)
 
+    def __hash__(self):
+        """
+        >>> a = {Literal('1',datatype=_XSD_NS.integer):'one'}
+        >>> Literal('1',datatype=_XSD_NS.double) in a
+        False
+        
+        [[
+        Called for the key object for dictionary operations, 
+        and by the built-in function hash(). Should return 
+        a 32-bit integer usable as a hash value for 
+        dictionary operations. The only required property 
+        is that objects which compare equal have the same 
+        hash value; it is advised to somehow mix together 
+        (e.g., using exclusive or) the hash values for the 
+        components of the object that also play a part in 
+        comparison of objects. 
+        ]] -- 3.4.1 Basic customization (Python)
+
+    
+        [[
+        Two literals are equal if and only if all of the following hold:
+        * The strings of the two lexical forms compare equal, character by character.
+        * Either both or neither have language tags.
+        * The language tags, if any, compare equal.
+        * Either both or neither have datatype URIs.
+        * The two datatype URIs, if any, compare equal, character by character.
+        ]] -- 6.5.1 Literal Equality (RDF: Concepts and Abstract Syntax)
+        
+        """
+        return hash(str(self)) ^ hash(self.language) ^ hash(self.datatype) 
+
     def __eq__(self, other):
         """        
         >>> f = URIRef("foo")
