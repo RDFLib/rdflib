0e02672ac76bd35003e48e4ce9de4c5f37d1070a
chimezie <devnull@localhost>
2007-10-26 02:24:33 +0000

    improved test coverage

--- test/test_sparql_date_filter.py
+++ test/test_sparql_date_filter.py
@@ -31,6 +31,28 @@ WHERE {
     FILTER (?date < xsd:dateTime("2006-01-01T00:00:00") && 
             ?date > xsd:dateTime("1995-06-15T00:00:00")) }"""
 
+QUERY2 = u"""
+PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+PREFIX   dc: <http://purl.org/dc/elements/1.1/>
+PREFIX  xsd: <http://www.w3.org/2001/XMLSchema#>
+SELECT ?doc
+WHERE { 
+  ?doc a foaf:Document;
+       dc:date ?date. 
+    FILTER (?date < "2006-01-01T00:00:00" && 
+            ?date > "1995-06-15T00:00:00") }"""
+
+QUERY3 = u"""
+PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+PREFIX   dc: <http://purl.org/dc/elements/1.1/>
+PREFIX  xsd: <http://www.w3.org/2001/XMLSchema#>
+SELECT ?doc
+WHERE { 
+  ?doc a foaf:Document;
+       dc:date ?date. 
+    FILTER (?date < "2006-01-01T00:00:00"^^xsd:dateTime && 
+            ?date > "1995-06-15T00:00:00"^^xsd:dateTime ) }"""
+
 ANSWER1 = URIRef('http://del.icio.us/rss/chimezie/paper')
 
 class DateFilterTest(unittest.TestCase):
@@ -38,14 +60,16 @@ class DateFilterTest(unittest.TestCase):
         self.graph = ConjunctiveGraph()
         self.graph.load(StringIO(testContent), format='n3')
     def test_DATE_FILTER1(self):
-        pQuery = Parse(QUERY1)
-        #print RenderSPARQLAlgebra(pQuery)
-        results = self.graph.query(pQuery,
-                                   DEBUG=False).serialize(format='python')
-        results = list(results)
-        self.failUnless(
-            len(results) and results == [ANSWER1],
-            "expecting : %s .  Got: %s"%([ANSWER1],repr(results)))
+        for query in [QUERY1,QUERY2,QUERY3]:
+            print query
+            pQuery = Parse(query)
+            #print RenderSPARQLAlgebra(pQuery)
+            results = self.graph.query(pQuery,
+                                       DEBUG=False).serialize(format='python')
+            results = list(results)
+            self.failUnless(
+                len(results) and results == [ANSWER1],
+                "expecting : %s .  Got: %s"%([ANSWER1],repr(results)))
 
 if __name__ == "__main__":
     unittest.main()
