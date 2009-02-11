d03743c423ebe05405a60148f2fddbd92d4f5516
eikeon@eikeon.com <eikeon@eikeon.com>
2009-02-11 22:15:48 +0000

    applied patch to add unicode escaping; closes issue #40

--- rdflib/syntax/serializers/NTSerializer.py
+++ rdflib/syntax/serializers/NTSerializer.py
@@ -15,5 +15,22 @@ class NTSerializer(Serializer):
             print "TODO: NTSerializer does not support base"
         encoding = self.encoding
         write = lambda triple: stream.write((triple[0].n3() + u" " + \
-                                             triple[1].n3() + u" " + triple[2].n3() + u".\n").encode(encoding, "replace"))
+                                             triple[1].n3() + u" " + _xmlcharref_encode(triple[2].n3()) + u".\n").encode(encoding, "replace"))
         map(write, self.store)
+
+
+# from http://code.activestate.com/recipes/303668/
+
+
+def _xmlcharref_encode(unicode_data, encoding="ascii"):
+    """Emulate Python 2.3's 'xmlcharrefreplace' encoding error handler."""
+    chars = []
+    # Step through the unicode_data string one character at a time in
+    # order to catch unencodable characters:
+    for char in unicode_data:
+        try:
+            chars.append(char.encode(encoding, 'strict'))
+        except UnicodeError:
+            chars.append('\u%04X' % ord(char))
+    return ''.join(chars)
+
