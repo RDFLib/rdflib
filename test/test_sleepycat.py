4fdd59b7ed2dd5bd016f2b67d14b0e6074970b74
eikeon <devnull@localhost>
2009-03-24 23:23:57 +0000

    cleaned up

--- test/test_sleepycat.py
+++ test/test_sleepycat.py
@@ -2,26 +2,15 @@ import logging
 
 _logger = logging.getLogger(__name__)
 
-from test.graph import GraphTestCase
-
-try:
-    from rdflib.store.Sleepycat import Sleepycat
-    class SleepycatGraphTestCase(GraphTestCase):
-        store_name = "Sleepycat"
-except ImportError, e:
-    _logger.warning("Can not test Sleepycat store: %s" % e)
+from test import test_graph
+from test import test_context
 
 
-from test.context import ContextTestCase
-
-try:
-    from rdflib.store.Sleepycat import Sleepycat
-    class SleepycatStoreTestCase(ContextTestCase):
-        store = "Sleepycat"
-except ImportError, e:
-    _logger.warning("Can not test Sleepycat store: %s" % e)
+class SleepycatGraphTestCase(test_graph.GraphTestCase):
+    store_name = "Sleepycat"
+    slowtest = True
 
 
-#class Sleepycat(PychinkoTestCase):
-#    backend = 'Sleepycat'
-
+class SleepycatStoreTestCase(test_context.ContextTestCase):
+    store = "Sleepycat"
+    slowtest = True
