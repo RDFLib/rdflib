cc68b14f68a6c44dd97a98714140a836eefe03a0
John.L.Clark <devnull@localhost>
2009-04-08 20:43:58 +0000

    Merge from Cleveland Clinic Mercurial maintenance branch.
    Original metadata, including description:
    
    changeset:   f0003306ab67
    user:        Brian Beck <exogen@gmail.com>
    date:        Tue Mar 24 15:32:44 2009 -0400
    description:
    Added test for SPARQL filter extension functions.

---
+++ test/test_sparql/test_filter_extensions.py
@@ -0,0 +1,41 @@
+from rdflib import ConjunctiveGraph, Namespace, BNode, RDF
+from rdflib.Literal import Literal, _XSD_NS
+
+DC = Namespace(u"http://purl.org/dc/elements/1.1/")
+FUNC = Namespace(u"http://example.org/functions#")
+
+graph = ConjunctiveGraph()
+graph.add((BNode(), RDF.value, Literal(0)))
+graph.add((BNode(), RDF.value, Literal(1)))
+graph.add((BNode(), RDF.value, Literal(2)))
+graph.add((BNode(), RDF.value, Literal(3)))
+
+from rdflib.Literal import _toPythonMapping
+NUMERIC_TYPES = [type_uri for type_uri in _toPythonMapping if \
+                 _toPythonMapping[type_uri] in (int, float, long)]
+
+def func_even(a):
+    # Should this be required, or be done automatically?
+    from rdflib.sparql.sparqlOperators import getValue
+    value = getValue(a)
+
+    if isinstance(value, Literal) and value.datatype in NUMERIC_TYPES:
+        return Literal(int(value.toPython() % 2 == 0), datatype=_XSD_NS.boolean)
+    else:
+        raise TypeError(a)
+
+def test_even_extension():
+    res = list(graph.query("""
+    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    PREFIX func:  <http://example.org/functions#>
+
+    SELECT ?value
+    WHERE { ?x rdf:value ?value . FILTER ( func:even(?value) ) }
+
+    """))
+    res.sort()
+    expected = [Literal(0), Literal(2)]
+    assert res == expected, "Expected %s but got %s" % (expected, res)
+
+if __name__ == '__main__':
+    test_even_extension()
