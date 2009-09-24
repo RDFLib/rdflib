95c67c4a70be97c2f4206211abba7e1bc97a430e
eikeon <devnull@localhost>
2009-09-24 22:13:48 +0000

    update issue 84

--- test/test_issue084.py
+++ test/test_issue084.py
@@ -1,6 +1,8 @@
+from codecs import getreader
+from StringIO import StringIO
+
 from rdflib.term import URIRef
 from rdflib.graph import Graph
-
 
 rdf = u"""@prefix skos:
 <http://www.w3.org/2004/02/skos/core#> .
@@ -15,11 +17,30 @@ rdf = u"""@prefix skos:
 :CI rdf:type skos:Concept;
     skos:prefLabel "C\u00f4te d'Ivoire"@en;
     skos:broaderTransitive :africa.    
-""".encode('utf-8')
+"""
+
+rdf_utf8 = rdf.encode('utf-8')
+
+rdf_reader = getreader('utf-8')(StringIO(rdf.encode('utf-8')))
+
+
         
-def test_issue():
+def test_a():
     g = Graph()
     g.parse(data=rdf, format='n3')
     v = g.value(subject=URIRef("http://www.test.org/#CI"), predicate=URIRef("http://www.w3.org/2004/02/skos/core#prefLabel"))
     assert v==u"C\u00f4te d'Ivoire"
+
+def test_b():
+    g = Graph()
+    g.parse(data=rdf_utf8, format='n3')
+    v = g.value(subject=URIRef("http://www.test.org/#CI"), predicate=URIRef("http://www.w3.org/2004/02/skos/core#prefLabel"))
+    assert v==u"C\u00f4te d'Ivoire"
+
+def test_c():
+    g = Graph()
+    g.parse(source=rdf_reader, format='n3')
+    v = g.value(subject=URIRef("http://www.test.org/#CI"), predicate=URIRef("http://www.w3.org/2004/02/skos/core#prefLabel"))
+    assert v==u"C\u00f4te d'Ivoire"
+
 
