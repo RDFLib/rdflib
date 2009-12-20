16fd32d89dc62096bf5fdc340ffaa86de08d2c9e
ed.summers <devnull@localhost>
2009-12-20 06:47:23 +0000

    only execute lax tests if html5lib is installed

--- test/rdfa/non_xhtml.py
+++ test/rdfa/non_xhtml.py
@@ -6,19 +6,29 @@ class NonXhtmlTest(TestCase):
     """
     RDFa that is in not well-formed XHTML is passed through html5lib. 
     These tests make sure that this RDFa an be processed both from 
-    a file, and from a URL.
+    a file, and from a URL. We can only run these tests if html5lib
+    is installed. Current html5lib isn't a dependency.
     """
 
     def test_url(self):
-        g = ConjunctiveGraph()
-        g.parse(location='http://oreilly.com/catalog/9780596516499/',
-                format='rdfa', 
-                lax=True)
-        self.assertTrue(len(g) > 0)
+        if self.html5lib_installed():
+            g = ConjunctiveGraph()
+            g.parse(location='http://oreilly.com/catalog/9780596516499/',
+                    format='rdfa', 
+                    lax=True)
+            self.assertTrue(len(g) > 0)
 
     def test_file(self):
-        g = ConjunctiveGraph()
-        g.parse(location='test/rdfa/oreilly.html',
-                format='rdfa',
-                lax=True)
-        self.assertEqual(len(g), 77)
+        if self.html5lib_installed():
+            g = ConjunctiveGraph()
+            g.parse(location='test/rdfa/oreilly.html',
+                    format='rdfa',
+                    lax=True)
+            self.assertEqual(len(g), 77)
+
+    def html5lib_installed(self):
+        try:
+            import html5lib
+            return True
+        except: 
+            return False
