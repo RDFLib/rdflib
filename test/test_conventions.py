83887b05cc12e9995f230c40e17493fae9750999
eikeon <devnull@localhost>
2010-02-02 17:47:30 +0000

    test_module_names now recurses

--- test/test_conventions.py
+++ test/test_conventions.py
@@ -1,6 +1,6 @@
 import unittest
 import pkgutil
-#import inspect
+import os.path
 
 import rdflib
 
@@ -8,11 +8,19 @@ import rdflib
 class A(unittest.TestCase):
 
 
-    def test_module_names(self, module=rdflib):
-        for importer, name, ispkg in pkgutil.iter_modules(rdflib.__path__):
+    def test_module_names(self, path=None):
+        if path is None:
+            path = rdflib.__path__
+
+            # TODO: handle cases where len(path) is not 1
+            assert len(path)==1, "We're assuming the path has exactly one item in it for now"
+            path = path[0]
+
+        for importer, name, ispkg in pkgutil.iter_modules([path]):
             if ispkg:
-                pass # TODO: complete me ;)
+                self.test_module_names(path=os.path.join(path, name))
             else:
+                print "checking:", name
                 self.assert_(name==name.lower(), "module name '%s' is not lower case" % name)
 
 
