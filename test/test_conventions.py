6fca188480df0bdf4f4fb40cab885ac9ab606276
eikeon <devnull@localhost>
2010-02-04 20:36:28 +0000

    updated to display all non lower case module names (not just the first one)

--- test/test_conventions.py
+++ test/test_conventions.py
@@ -6,11 +6,12 @@ import rdflib
 
 
 class A(unittest.TestCase):
-    known_issue = True
 
-    def test_module_names(self, path=None):
+    def module_names(self, path=None, names=None):
         if path is None:
             path = rdflib.__path__
+        if names is None:
+            names = set()
 
             # TODO: handle cases where len(path) is not 1
             assert len(path)==1, "We're assuming the path has exactly one item in it for now"
@@ -18,9 +19,18 @@ class A(unittest.TestCase):
 
         for importer, name, ispkg in pkgutil.iter_modules([path]):
             if ispkg:
-                self.test_module_names(path=os.path.join(path, name))
+                result = self.module_names(path=os.path.join(path, name), 
+                                           names=names)
+                names.union(result)
             else:
-                self.assert_(name==name.lower(), "module name '%s' is not lower case" % name)
+                if name!=name.lower():
+                    names.add(name)
+                #self.assert_(name==name.lower(), "module name '%s' is not lower case" % name)
+        return names
+
+    def test_module_names(self):
+        names = self.module_names()
+        self.assert_(names==set(), "module names '%s' are not lower case" % names)
 
 
 if __name__ == "__main__":
