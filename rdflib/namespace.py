3c7e0e4d61b16497562c4a66f8e1ef26f2955836
lindstream <devnull@localhost>
2010-03-04 12:37:49 +0000

    added rdf:PlainLiteral to the RDF ClosedNamespace

--- rdflib/namespace.py
+++ rdflib/namespace.py
@@ -50,7 +50,7 @@ class NamespaceDict(dict):
             if self.__context and (uri, None, None) not in self.__context:
                 _logger.warning("%s not defined" % uri)
             self[name] = uri
-        return uri 
+        return uri
 
     def __getattr__(self, name):
         return self.term(name)
@@ -67,7 +67,7 @@ class NamespaceDict(dict):
 
 class ClosedNamespace(object):
     """
-    
+
     """
 
     def __init__(self, uri, terms):
@@ -102,19 +102,19 @@ class ClosedNamespace(object):
 class _RDFNamespace(ClosedNamespace):
     def __init__(self):
         super(_RDFNamespace, self).__init__(
-            URIRef("http://www.w3.org/1999/02/22-rdf-syntax-ns#"), 
+            URIRef("http://www.w3.org/1999/02/22-rdf-syntax-ns#"),
             terms=[
         # Syntax Names
-        "RDF", "Description", "ID", "about", "parseType", "resource", "li", "nodeID", "datatype", 
-        
+        "RDF", "Description", "ID", "about", "parseType", "resource", "li", "nodeID", "datatype",
+
         # RDF Classes
-        "Seq", "Bag", "Alt", "Statement", "Property", "XMLLiteral", "List", 
-        
+        "Seq", "Bag", "Alt", "Statement", "Property", "XMLLiteral", "List", "PlainLiteral",
+
         # RDF Properties
-        "subject", "predicate", "object", "type", "value", "first", "rest", 
+        "subject", "predicate", "object", "type", "value", "first", "rest",
         # and _n where n is a non-negative integer
-        
-        # RDF Resources          
+
+        # RDF Resources
         "nil"]
             )
 
@@ -124,14 +124,14 @@ class _RDFNamespace(ClosedNamespace):
             return URIRef("%s_%s" % (self.uri, i))
         except ValueError, e:
             return super(_RDFNamespace, self).term(name)
-        
+
 RDF = _RDFNamespace()
 
 RDFS = ClosedNamespace(
-    uri = URIRef("http://www.w3.org/2000/01/rdf-schema#"), 
+    uri = URIRef("http://www.w3.org/2000/01/rdf-schema#"),
     terms = [
-        "Resource", "Class", "subClassOf", "subPropertyOf", "comment", "label", 
-        "domain", "range", "seeAlso", "isDefinedBy", "Literal", "Container", 
+        "Resource", "Class", "subClassOf", "subPropertyOf", "comment", "label",
+        "domain", "range", "seeAlso", "isDefinedBy", "Literal", "Container",
         "ContainerMembershipProperty", "member", "Datatype"]
     )
 
@@ -166,7 +166,7 @@ class NamespaceManager(object):
     def normalizeUri(self,rdfTerm):
         """
         Takes an RDF Term and 'normalizes' it into a QName (using the registered prefix)
-        or (unlike compute_qname) the Notation 3 form for URIs: <...URI...> 
+        or (unlike compute_qname) the Notation 3 form for URIs: <...URI...>
         """
         try:
             namespace, name = split_uri(rdfTerm)
@@ -182,8 +182,8 @@ class NamespaceManager(object):
         elif prefix is None:
             return "<%s>"%rdfTerm
         else:
-            qNameParts = self.compute_qname(rdfTerm)         
-            return ':'.join([qNameParts[0],qNameParts[-1]])    
+            qNameParts = self.compute_qname(rdfTerm)
+            return ':'.join([qNameParts[0],qNameParts[-1]])
 
     def compute_qname(self, uri):
         if not uri in self.__cache:
