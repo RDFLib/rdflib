ba33025a2ad6b0392e9f4d3a26252a57d0ac9431
Graham Higgins <gjh@bel-epa.com>
2012-01-06 20:03:40 +0000

    Integrate nose Python 3 tests

--- setup.py
+++ setup.py
@@ -2,12 +2,47 @@
 import sys
 import re
 
-from distutils.core import setup
+def setup_python3():
+    # Taken from "distribute" setup.py
+    from distutils.filelist import FileList
+    from distutils import dir_util, file_util, util, log
+    from os.path import join
+  
+    tmp_src = join("build", "src")
+    log.set_verbosity(1)
+    fl = FileList()
+    for line in open("MANIFEST.in"):
+        if not line.strip():
+            continue
+        fl.process_template_line(line)
+    dir_util.create_tree(tmp_src, fl.files)
+    outfiles_2to3 = []
+    for f in fl.files:
+        outf, copied = file_util.copy_file(f, join(tmp_src, f), update=1)
+        if copied and outf.endswith(".py"):
+            outfiles_2to3.append(outf)
+  
+    util.run_2to3(outfiles_2to3)
+  
+    # arrange setup to use the copy
+    sys.path.insert(0, tmp_src)
+  
+    return tmp_src
 
 kwargs = {}
 if sys.version_info[0] >= 3:
     from setuptools import setup
     kwargs['use_2to3'] = True
+    kwargs['requires'] = ['bsddb3']
+    kwargs['src_root'] = setup_python3()
+else:
+    try:
+        from setuptools import setup
+        kwargs['src_root'] = "."
+        kwargs['test_suite'] = "nose.collector"
+    except ImportError:
+        from distutils.core import setup
+
 
 # Find version. We have to do this because we can't import it in Python 3 until
 # its been automatically converted in the setup process.
@@ -19,7 +54,6 @@ def find_version(filename):
             return version_match.group(1)
 
 version = find_version('rdflib/__init__.py')
-
 
 setup(
     name = 'rdflib',
@@ -33,6 +67,12 @@ setup(
     license = "http://rdflib.net/latest/LICENSE",
     platforms = ["any"],
     classifiers = ["Programming Language :: Python",
+                   "Programming Language :: Python :: 2",
+                   "Programming Language :: Python :: 3",
+                   "Programming Language :: Python :: 2.5",
+                   "Programming Language :: Python :: 2.6",
+                   "Programming Language :: Python :: 2.7",
+                   "Programming Language :: Python :: 3.2",
                    "License :: OSI Approved :: BSD License",
                    "Topic :: Software Development :: Libraries :: Python Modules",
                    "Operating System :: OS Independent",
@@ -52,7 +92,6 @@ setup(
     http://rdflib.googlecode.com/hg#egg=rdflib-dev
     """,
     download_url = "http://rdflib.net/rdflib-%s.tar.gz" % version,
-
     packages = ['rdflib',
                 'rdflib/plugins',
                 'rdflib/plugins',
@@ -63,3 +102,4 @@ setup(
                 ],
     **kwargs
     )
+
