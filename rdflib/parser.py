899a86f7563c3c92cf5352e64ffed275c1b80a1c
Graham Higgins <gjh-github@bel-epa.com>
2012-02-20 15:19:43 +0000

    Hopeful fix for https://github.com/RDFLib/rdflib/issues/130

--- rdflib/parser.py
+++ rdflib/parser.py
@@ -91,6 +91,9 @@ class URLInputSource(InputSource):
         
         req = Request(system_id, None, myheaders)
         file = urlopen(req)
+        # Fix for issue 130 https://github.com/RDFLib/rdflib/issues/130
+        self.url = file.geturl()    # in case redirections took place
+        self.setPublicId(self.url)
         self.content_type = file.info().get('content-type')
         self.content_type = self.content_type.split(";", 1)[0]
         self.setByteStream(file)
@@ -144,6 +147,8 @@ def create_input_source(source=None, publicID=None,
             else:
                 raise Exception("Unexpected type '%s' for source '%s'" % (type(source), source))
 
+    absolute_location = None # Further to fix for issue 130
+
     if location is not None:
         # Fix for Windows problem https://github.com/RDFLib/rdflib/issues/145
         if os.path.exists(location):
@@ -155,7 +160,7 @@ def create_input_source(source=None, publicID=None,
             file = open(filename, "rb")
         else:
             input_source = URLInputSource(absolute_location, format)
-        publicID = publicID or absolute_location
+        # publicID = publicID or absolute_location # Further to fix for issue 130
 
     if file is not None:
         input_source = FileInputSource(file)
@@ -168,13 +173,11 @@ def create_input_source(source=None, publicID=None,
     if input_source is None:
         raise Exception("could not create InputSource")
     else:
-        if publicID:
+        if publicID is not None: # Further to fix for issue 130
             input_source.setPublicId(publicID)
-
-        # TODO: what motivated this bit?
-        id = input_source.getPublicId()
-        if id is None:
-            input_source.setPublicId("")
+        # Further to fix for issue 130
+        elif input_source.getPublicId() is None:
+            input_source.setPublicId(absolute_location or "")
         return input_source
 
 
