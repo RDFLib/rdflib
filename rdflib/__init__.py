c6cd10fdceb77788147b4e2b8a0aeb1365846ab8
Graham Higgins <gjh-github@bel-epa.com>
2012-03-27 20:37:49 +0100

    Use URI that points to unchanging n3 content.

--- rdflib/__init__.py
+++ rdflib/__init__.py
@@ -15,10 +15,10 @@ A tiny example:
     >>> import rdflib
 
     >>> g = rdflib.Graph()
-    >>> result = g.parse("http://www.w3.org/People/Berners-Lee/card")
+    >>> result = g.parse("http://www.w3.org/2000/10/swap/test/meet/white.rdf")
 
     >>> print("graph has %s statements." % len(g))
-    graph has 77 statements.
+    graph has 19 statements.
     >>>
     >>> for s, p, o in g:
     ...     if (s, p, o) not in g:
