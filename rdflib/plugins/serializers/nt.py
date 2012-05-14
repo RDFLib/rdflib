30edcb58a5b96f8413787dd792d28cfb7f6a074e
Bernhard Schandl <bernhard.schandl@gmail.com>
2012-05-14 15:44:34 +0200

    fix nt serialization

--- rdflib/plugins/serializers/nt.py
+++ rdflib/plugins/serializers/nt.py
@@ -38,26 +38,44 @@ def _xmlcharref_encode(unicode_data, encoding="ascii"):
     # nothing to do about xmlchars, but replace newlines with escapes: 
     unicode_data=unicode_data.replace("\n","\\n")
     if unicode_data.startswith('"""'):
+
         # Updated with Bernhard Schandl's patch...
         # unicode_data = unicode_data.replace('"""', '"')   # original
+
+        # print "input: '%s'" % unicode_data
+        # print "input len: %d" % len(unicode_data)
 
         last_triplequote_pos = unicode_data.rfind('"""')
         payload = unicode_data[3:last_triplequote_pos]
         trail = unicode_data[last_triplequote_pos+3:]
 
+        # print "payload: '%s'" % payload
+        # print "payload len: %d" % len(payload)
+
+        # replace carriage returns
+        payload = payload.replace('\r', '\\r')
+
         # fix three-quotes encoding
         payload = payload.replace('\\"""', '"""')
+        # print "payload after 3quote fixing: '%s'" % payload
 
         # corner case: if string ends with " it is already encoded.
         # so we need to de-escape it before it will be re-escaped in the next step.
+        payload = payload.replace('\\"', '"')
+        # print "payload after de-escaping: '%s'" % payload
+
+        # extra corner case: if string still ends with escaped quotation mark, we need to get that away
         if payload.endswith('\\"'):
-            payload = payload.replace('\\"', '"')
+            payload = payload[:-2] + '"'
+            # print "payload after end-de-escaping: '%s'" % payload
 
         # escape quotes in payload
         payload = payload.replace('"', '\\"')
+        # print "payload after replacing quotes: '%s'" % payload
 
         # reconstruct result using single quotes
         unicode_data = '"%s"%s' % (payload, trail)
+        # print "result: '%s'" % unicode_data
 
     # Step through the unicode_data string one character at a time in
     # order to catch unencodable characters:                          
