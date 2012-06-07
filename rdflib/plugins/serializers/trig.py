ea174256a185c7eb9cdb1030caeb20794b9e3800
Graham Higgins <gjh-github@bel-epa.com>
2012-06-07 01:10:11 +0100

    Pull-by-hand from mammadori's repos commit https://github.com/mammadori/rdflib/commit/494429bfad09532b33eee49d197cba997b59f277

--- rdflib/plugins/serializers/trig.py
+++ rdflib/plugins/serializers/trig.py
@@ -1,21 +1,61 @@
-from rdflib.plugin import register
-try:
-    from rdflib.serializer import Serializer
-    from rdflib.plugins.serializers.n3 import N3Serializer
-except ImportError:
-    from rdflib.syntax.serializer import Serializer
-    from rdflib.syntax.serializers.N3Serializer import N3Serializer
-from rdflib import BNode
-from StringIO import StringIO
-import sys
+"""
+Trig RDF graph serializer for RDFLib.
+See <http://www.w3.org/2010/01/Trig/Trig> for syntax specification.
+"""
 
-__all__ = ["Serializer", "TriGSerializer"]
+from rdflib.plugins.serializers.turtle import TurtleSerializer
+from rdflib.plugins.serializers.turtle import _GEN_QNAME_FOR_DT
+from rdflib.plugins.serializers.turtle import VERB
 
-class TriGSerializer(N3Serializer):
+from rdflib.term import BNode, Literal
+
+from collections import defaultdict
+
+
+__all__ = ['TrigSerializer']
+
+
+class TrigSerializer(TurtleSerializer):
+
+    short_name = "trig"
+    indentString = 4 * u' '
+
+    def __init__(self, store):
+        if store.context_aware:
+            self.contexts = store.contexts()
+        else:
+            self.contexts = [store]
+
+        super(TrigSerializer, self).__init__(store)
+
+    def preprocess(self):
+        for context in self.contexts:
+            for triple in context:
+                self.preprocessTriple(triple, context.identifier)
+
+    def preprocessTriple(self, triple, identifier):
+        s, p, o = triple
+        references = self.refCount(o) + 1
+        self._references[o] = references
+        self._subjects[s] = True
+        self._contexts[identifier].add(s)
+        for i, node in enumerate(triple):
+            if node in self.keywords:
+                continue
+            # Don't use generated prefixes for subjects and objects
+            self.getQName(node, gen_prefix=(i == VERB))
+            if isinstance(node, Literal) and node.datatype:
+                self.getQName(node.datatype, gen_prefix=_GEN_QNAME_FOR_DT)
+        p = triple[1]
+        if isinstance(p, BNode):
+            self._references[p] = self.refCount(p) + 1
+
+    def reset(self):
+        super(TrigSerializer, self).reset()
+        self._contexts = defaultdict(set)
+
     def serialize(self, stream, base=None, encoding=None, spacious=None, **args):
         self.reset()
-        if stream is None:
-                stream = StringIO()
         self.stream = stream
         self.base = base
 
@@ -23,24 +63,24 @@ class TriGSerializer(N3Serializer):
             self._spacious = spacious
 
         self.preprocess()
+        # @@FIXME: Unused code ...
+        # subjects_list = self.orderSubjects()
+
         self.startDocument()
 
-        if self.store.context_aware:
-            contexts = self.store.contexts()
-        else:
-            contexts = [self.store]
-        for g in contexts:
-            self.write("\n%s {" % self.store.qname(g.identifier))
+        firstTime = True
+        for identifier, subjects in self._contexts.items():
+            self.write(self.indent() + '\n<%s> = {' % identifier)
             self.depth += 1
-            s = N3Serializer(g, parent=self)
-            s.serialize(self.stream)
+            for subject in subjects:
+                if self.isDone(subject):
+                    continue
+                if firstTime:
+                    firstTime = False
+                if self.statement(subject) and not firstTime:
+                    self.write('\n')
             self.depth -= 1
-            self.write("}\n")
+            self.write('}\n')
 
         self.endDocument()
-        self.write("\n")
-      
-        if isinstance(self.stream, StringIO): 
-            return self.stream.getvalue()
-
-
+        stream.write(u"\n".encode('ascii'))
