f80c92ac1ebb87f3f7c0a3ce6648cec3cf50fb2a
DzinX <dzinxed@gmail.com>
2012-09-21 12:09:02 +0200

    Changed assertIsNotNone to assertFalse
    assertIsNotNone is not compatible with Python < 2.7

--- test/test_trig_serialize.py
+++ test/test_trig_serialize.py
@@ -6,7 +6,7 @@ class TestTrigSerialize(unittest.TestCase):
     def test_empty_graph(self):
         graph = ConjunctiveGraph()
         text = graph.serialize(format='trig')
-        self.assertIsNotNone(text)
+        self.assertFalse(text is None)
 
 if __name__ == "__main__":
     unittest.main()
