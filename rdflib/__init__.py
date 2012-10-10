05ca4e7825df4a400434a01857ae3c4a5b90cec2
gromgull <gromgull@gmail.com>
2012-10-10 11:28:19 +0200

    set version number 3.2.3

--- rdflib/__init__.py
+++ rdflib/__init__.py
@@ -30,8 +30,8 @@ A tiny example:
 __docformat__ = "restructuredtext en"
 
 # The format of the __version__ line is matched by a regex in setup.py
-__version__ = "3.4.0-dev"
-__date__ = "2012/09/25"
+__version__ = "3.2.3"
+__date__ = "2012/10/10"
 
 __all__ = [
     'URIRef',
