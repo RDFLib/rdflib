4e29d26b1f60c4fb15e78388fc7d53b68dd45604
Graham Higgins <gjh-github@bel-epa.com>
2013-03-20 14:37:00 +0000

    Clean up for a couple of tests

--- test/test_issue247.py
+++ test/test_issue247.py
@@ -1,8 +1,7 @@
 import rdflib
 import unittest
-import threading
 
-failxml = """
+failxml = """\
 <rdf:RDF
     xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
     xmlns:dc="http://purl.org/dc/elements/1.1/"
@@ -16,7 +15,7 @@ failxml = """
 
 </rdf:RDF>"""
 
-passxml = """
+passxml = """\
 <rdf:RDF
     xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
     xmlns:dc="http://purl.org/dc/elements/1.1/"
@@ -31,16 +30,23 @@ passxml = """
 </rdf:RDF>"""
 
 
-class TestRandomSeedInThread(unittest.TestCase):
+class TestXMLLiteralwithLangAttr(unittest.TestCase):
 
-    def test_parse_of_literal_with_xmllang_attr(self):
+    def test_successful_parse_of_literal_without_xmllang_attr(self):
         """
+        Test parse of Literal without xmllang attr passes
         Parsing an RDF/XML document fails with a KeyError when
         it contains a XML Literal with a xml:lang attribute:
         """
         g = rdflib.Graph()
         g.parse(data=passxml)
-        del g
+
+    def test_failing_parse_of_literal_with_xmllang_attr(self):
+        """
+        Show parse of Literal with xmllang attr fails
+        Parsing an RDF/XML document fails with a KeyError when
+        it contains a XML Literal with a xml:lang attribute:
+        """
         g = rdflib.Graph()
         g.parse(data=failxml)
 
