09ad03f912a4d3cf6abeed8a991e5177d005cfa1
Gunnar Aastrand Grimnes <gromgull@gmail.com>
2013-05-02 07:55:12 +0200

    aaaand one more py3 unicode fix, really fixes #270 :)

--- rdflib/py3compat.py
+++ rdflib/py3compat.py
@@ -128,4 +128,13 @@ def decodeStringEscape(s):
     if not PY3:
         return s.decode('string-escape')
     else:
+        s = s.replace('\\t', '\t')
+        s = s.replace('\\n', '\n')
+        s = s.replace('\\r', '\r')
+        s = s.replace('\\b', '\b')
+        s = s.replace('\\f', '\f')
+        s = s.replace('\\"', '"')
+        s = s.replace("\\'", "'")
+        s = s.replace('\\\\', '\\')
+
         return re.sub(r'(\\u[0-9A-Fa-f]+)', lambda m: chr(int(m.group(0)[2:], 16)), s)
