7e1b102676f6c648d0625ac285b345f2db1bc5b3
Gunnar Aastrand Grimnes <gromgull@gmail.com>
2013-12-18 07:10:48 +0100

    added trig tests, re #342 and fixes #317 - but many of the tests do not pass :)

---
+++ test/test_trig_w3c.py
@@ -0,0 +1,31 @@
+"""Runs the tests for the W3C RDF Working Group's TriG test suite.
+
+"""
+
+from rdflib import Graph
+from manifest import nose_tests, RDFT
+
+
+def trig(test):
+    g = Graph()
+
+    try:
+        g.parse(test.action, format='trig')
+        if not test.syntax:
+            raise AssertionError("Input shouldn't have parsed!")
+    except:
+        if test.syntax:
+            raise
+
+testers = {
+    RDFT.TestTrigPositiveSyntax: trig,
+    RDFT.TestTrigNegativeSyntax: trig
+}
+
+def test_trig():
+    for t in nose_tests(testers, 'test/trig/manifest.ttl'):
+        yield t
+
+
+if __name__ == '__main__':
+    pass
