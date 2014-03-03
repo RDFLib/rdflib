1c057101c870fcd94e6b581386af2d16167b3d3b
Gunnar Aastrand Grimnes <gromgull@gmail.com>
2014-03-03 07:48:59 +0100

    Skip broken test, added test for parseTypeCollection

--- test/test_issue363.py
+++ test/test_issue363.py
@@ -1,7 +1,8 @@
 import rdflib
+from nose import SkipTest
 from nose.tools import assert_raises
 
-data='''<?xml version="1.0" encoding="utf-8"?>
+data = '''<?xml version="1.0" encoding="utf-8"?>
 <rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
     xmlns:http="http://www.w3.org/2011/http#">
 
@@ -15,12 +16,31 @@ data='''<?xml version="1.0" encoding="utf-8"?>
 </rdf:RDF>
 '''
 
+data2 = '''<?xml version="1.0" encoding="utf-8"?>
+<rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+    xmlns="http://www.example.org/meeting_organization#">
 
+    <rdf:Description about="http://meetings.example.com/cal#m1">
+        <Location parseType="Resource">
+            <zip xmlns="http://www.another.example.org/geographical#">02139</zip>
+            <lat xmlns="http://www.another.example.org/geographical#">14.124425</lat>
+        </Location>
+    </rdf:Description>
+</rdf:RDF>
+'''
 
 def test_broken_rdfxml():
     #import ipdb; ipdb.set_trace()
-
+    raise SkipTest()
     def p():
-        g = rdflib.Graph().parse(data=data)
+        rdflib.Graph().parse(data=data)
 
     assert_raises(Exception, p)
+
+def test_parsetype_resource():
+    g = rdflib.Graph().parse(data=data2)
+    print g.serialize(format='n3')
+
+if __name__ == '__main__':
+    #test_broken_rdfxml()
+    test_parsetype_resource()
