661c71208d44822f1ad697bef78e97edaecab0f0
Joern Hees <dev@joernhees.de>
2014-03-18 11:04:42 +0100

    preparing use of six in setup code, removed py3 2to3 special handling

--- setup.py
+++ setup.py
@@ -4,46 +4,14 @@ import os
 import re
 
 
-def setup_python3():
-    # Taken from "distribute" setup.py
-    from distutils.filelist import FileList
-    from distutils import dir_util, file_util, util, log
-    from os.path import join, exists
-
-    tmp_src = join("build", "src")
-    # Not covered by "setup.py clean --all", so explicit deletion required.
-    if exists(tmp_src):
-        dir_util.remove_tree(tmp_src)
-    log.set_verbosity(1)
-    fl = FileList()
-    for line in open("MANIFEST.in"):
-        if not line.strip():
-            continue
-        fl.process_template_line(line)
-    dir_util.create_tree(tmp_src, fl.files)
-    outfiles_2to3 = []
-    for f in fl.files:
-        outf, copied = file_util.copy_file(f, join(tmp_src, f), update=1)
-        if copied and outf.endswith(".py"):
-            outfiles_2to3.append(outf)
-
-    util.run_2to3(outfiles_2to3)
-
-    # arrange setup to use the copy
-    sys.path.insert(0, tmp_src)
-
-    return tmp_src
-
 kwargs = {}
 if sys.version_info[0] >= 3:
     from setuptools import setup
-    kwargs['use_2to3'] = True
     kwargs['install_requires'] = ['isodate', 'pyparsing']
     kwargs['tests_require'] = ['html5lib']
     kwargs['requires'] = [
-        'isodate', 'pyparsing',
+        'six', 'isodate', 'pyparsing',
         'SPARQLWrapper']
-    kwargs['src_root'] = setup_python3()
     assert setup
 else:
     try:
@@ -51,7 +19,7 @@ else:
         assert setup
         kwargs['test_suite'] = "nose.collector"
         kwargs['install_requires'] = [
-            'isodate',
+            'six', 'isodate',
             'pyparsing', 'SPARQLWrapper']
 
         if sys.version_info[1]<7:  # Python 2.6
