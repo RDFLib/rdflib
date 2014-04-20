ed6943eb0b27489b08910053801abe10d50022ae
Joern Hees <dev@joernhees.de>
2014-04-21 00:18:25 +0200

    test-case for #382

--- test/test_rdf_lists.py
+++ test/test_rdf_lists.py
@@ -20,8 +20,19 @@ _:fIYNVPxd3 <http://www.w3.org/1999/02/22-rdf-syntax-ns#first> <http://example.c
 _:fIYNVPxd3 <http://www.w3.org/1999/02/22-rdf-syntax-ns#rest> <http://www.w3.org/1999/02/22-rdf-syntax-ns#nil>.
 """
 
+DATA_FALSE_ELEMENT=\
+"""
+<http://example.org/#ThreeMemberList> <http://www.w3.org/1999/02/22-rdf-syntax-ns#first> <http://example.org/#p> .
+<http://example.org/#ThreeMemberList> <http://www.w3.org/1999/02/22-rdf-syntax-ns#rest> _:list2 .
+_:list2 <http://www.w3.org/1999/02/22-rdf-syntax-ns#first> "false"^^<http://www.w3.org/2001/XMLSchema#boolean> .
+_:list2 <http://www.w3.org/1999/02/22-rdf-syntax-ns#rest> _:list3 .
+_:list3 <http://www.w3.org/1999/02/22-rdf-syntax-ns#first> <http://example.org/#r> .
+_:list3 <http://www.w3.org/1999/02/22-rdf-syntax-ns#rest> <http://www.w3.org/1999/02/22-rdf-syntax-ns#nil> .
+"""
+
 def main():
     unittest.main()
+
 
 class OWLCollectionTest(unittest.TestCase):
 
@@ -29,6 +40,13 @@ class OWLCollectionTest(unittest.TestCase):
         g=Graph().parse(data=DATA, format='nt')
         g.namespace_manager.bind('owl',URIRef('http://www.w3.org/2002/07/owl#'))
         print g.serialize(format='pretty-xml')
+
+
+class ListTest(unittest.TestCase):
+    def testFalseElement(self):
+        g=Graph().parse(data=DATA_FALSE_ELEMENT, format='nt')
+        self.assertEquals(len(list(g.items(URIRef('http://example.org/#ThreeMemberList')))), 3)
+
 
 if __name__ == '__main__':
     main()
