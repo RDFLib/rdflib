a7498c8e5335de28545d5ad8ebbe04fd79b431ea
Joern Hees <dev@joernhees.de>
2014-11-27 00:24:29 +0100

    six: __future__ headers

--- rdflib/term.py
+++ rdflib/term.py
@@ -20,6 +20,10 @@ underlying Graph:
 * Numerical Ranges
 
 """
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+# from __future__ import unicode_literals
 
 __all__ = [
     'bind',
