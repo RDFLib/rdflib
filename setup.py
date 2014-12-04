c837d55f2836c711049c5e70d9a16df97846a190
Joern Hees <dev@joernhees.de>
2014-12-04 16:41:41 +0100

    six: parsers/nquads.py: headers, exception syntax

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ def setup_python3():
         join(tmp_src, 'rdflib', 'plugins', 'parsers', '__init__.py'),
         join(tmp_src, 'rdflib', 'plugins', 'parsers', 'hturtle.py'),
         join(tmp_src, 'rdflib', 'plugins', 'parsers', 'notation3.py'),
-        # join(tmp_src, 'rdflib', 'plugins', 'parsers', 'nquads.py'),
+        join(tmp_src, 'rdflib', 'plugins', 'parsers', 'nquads.py'),
         join(tmp_src, 'rdflib', 'plugins', 'parsers', 'nt.py'),
         # join(tmp_src, 'rdflib', 'plugins', 'parsers', 'ntriples.py'),
         # join(tmp_src, 'rdflib', 'plugins', 'parsers', 'pyMicrodata', '__init__.py'),
