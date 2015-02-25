16b63f7631c43bfeb48e6946c0178322794623c4
Joern Hees <dev@joernhees.de>
2015-02-25 13:29:33 +0100

    made test for #457 py3 compatible

--- test/test_issue457.py
+++ test/test_issue457.py
@@ -5,8 +5,9 @@ from xml.etree import ElementTree
 
 import rdflib
 from rdflib.plugins.stores.sparqlstore import CastToTerm
+from rdflib.py3compat import b
 
-s = """<ns0:literal xmlns:ns0="http://www.w3.org/2005/sparql-results#" />"""
+s = b("""<ns0:literal xmlns:ns0="http://www.w3.org/2005/sparql-results#" />""")
 node = ElementTree.parse(io.BytesIO(s)).getroot()
 term = CastToTerm(node)
 assert term  == rdflib.Literal(''), repr(term)
