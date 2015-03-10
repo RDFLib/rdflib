0de105210f419cac4941e7c0da03cc2768cd8904
Joern Hees <dev@joernhees.de>
2015-03-10 22:34:44 +0100

    edge attributes as callables, transformation functions and refactoring

--- rdflib/extras/external_graph_libs.py
+++ rdflib/extras/external_graph_libs.py
@@ -14,15 +14,71 @@ Currently the following libraries are supported:
 import logging
 logger = logging.getLogger(__name__)
 
-def rdflib_to_networkx_multidigraph(graph, triple_attr=True, **edge_attrs):
+_identity = lambda x: x
+
+def _rdflib_to_networkx_graph(
+        graph,
+        nxgraph,
+        calc_weights,
+        edge_attrs,
+        transform_s=_identity, transform_o=_identity):
+    """Helper method for multidigraph, digraph and graph.
+
+    Modifies nxgraph in-place!
+
+    Arguments:
+        graph: an rdflib.Graph.
+        nxgraph: a networkx.Graph/DiGraph/MultiDigraph.
+        calc_weights: If True adds a 'weight' attribute to each edge according
+            to the count of s,p,o triples between s and o, which is meaningful
+            for Graph/DiGraph.
+        edge_attrs: Callable to construct edge data from s, p, o.
+           'triples' attribute is handled specially to be merged.
+           'weight' should not be generated if calc_weights==True.
+           (see invokers below!)
+        transform_s: Callable to transform node generated from s.
+        transform_o: Callable to transform node generated from o.
+    """
+    assert callable(edge_attrs)
+    assert callable(transform_s)
+    assert callable(transform_o)
+    import networkx as nx
+    for s, p, o in graph:
+        ts, to = transform_s(s), transform_o(o)  # apply possible transformations
+        data = nxgraph.get_edge_data(ts, to)
+        if data is None or isinstance(nxgraph, nx.MultiDiGraph):
+            # no edge yet, set defaults
+            data = edge_attrs(s, p, o)
+            if calc_weights:
+                data['weight'] = 1
+            nxgraph.add_edge(ts, to, **data)
+        else:
+            # already have an edge, just update attributes
+            if calc_weights:
+                data['weight'] += 1
+            if 'triples' in data:
+                d = edge_attrs(s, p, o)
+                data['triples'].extend(d['triples'])
+
+def rdflib_to_networkx_multidigraph(
+        graph,
+        edge_attrs=lambda s, p, o: {'key': p},
+        **kwds):
     """Converts the given graph into a networkx.MultiDiGraph.
 
     The subjects and objects are the later nodes of the MultiDiGraph.
     The predicates are used as edge keys (to identify multi-edges).
 
     Arguments:
-        triple_attr: Adds a 'triple' attribute to each edge.
-        **edge_attrs: Default edge attributes
+        graph: a rdflib.Graph.
+        edge_attrs: Callable to construct later edge_attributes. It receives
+            3 variables (s, p, o) and should construct a dictionary that is
+            passed to networkx's add_edge(s, o, **attrs) function.
+
+            By default this will include setting the MultiDiGraph key=p here.
+            If you don't want to be able to re-identify the edge later on, you
+            can set this to `lambda s, p, o: {}`. In this case MultiDiGraph's
+            default (increasing ints) will be used.
 
     Returns:
         networkx.MultiDiGraph
@@ -44,45 +100,23 @@ def rdflib_to_networkx_multidigraph(graph, triple_attr=True, **edge_attrs):
     True
     >>> mdg.has_edge(a, b, key=q)
     True
-    >>> mdg[a][b][p]['triple'] == (a, p, b)
+
+    >>> mdg = rdflib_to_networkx_multidigraph(g, edge_attrs=lambda s,p,o: {})
+    >>> mdg.has_edge(a, b, key=0)
+    True
+    >>> mdg.has_edge(a, b, key=1)
     True
     """
     import networkx as nx
     mdg = nx.MultiDiGraph()
-    for s, p, o in graph:
-        d = dict(**edge_attrs)
-        if triple_attr:
-            d['triple'] = (s, p, o)
-        mdg.add_edge(s, o, key=p, **d)
+    _rdflib_to_networkx_graph(graph, mdg, False, edge_attrs, **kwds)
     return mdg
 
-def _rdflib_to_networkx_graph(graph,
-                              nxgraph,
-                              calc_weights=True,
-                              triples_attr=True,
-                              **edge_attrs):
-    """Helper method for graph and digraph, modifies nxgraph in-place!"""
-    for s, p, o in graph:
-        data = nxgraph.get_edge_data(s, o)
-        if data is None:
-            # no edge yet, set defaults
-            data = dict(**edge_attrs)
-            if calc_weights:
-                data['weight'] = 1
-            if triples_attr:
-                data['triples'] = [(s, p, o)]
-            nxgraph.add_edge(s, o, data)
-        else:
-            # already have an edge, just update attributes
-            if calc_weights:
-                data['weight'] += 1
-            if triples_attr:
-                data['triples'].append((s, p, o))
-
-def rdflib_to_networkx_digraph(graph,
-                               calc_weights=True,
-                               triples_attr=True,
-                               **edge_attrs):
+def rdflib_to_networkx_digraph(
+        graph,
+        calc_weights=True,
+        edge_attrs=lambda s, p, o: {'triples': [(s, p, o)]},
+        **kwds):
     """Converts the given graph into a networkx.DiGraph.
 
     As an rdflib.Graph() can contain multiple edges between nodes, by default
@@ -91,9 +125,17 @@ def rdflib_to_networkx_digraph(graph,
     Also by default calculates the edge weight as the length of triples.
 
     Args:
+        graph: a rdflib.Graph.
         calc_weights: If true calculate multi-graph edge-count as edge 'weight'
-        triples_attr: If true each edge will have a 'triples' attr.
-        **edge_attrs: Attributes added to each edge.
+        edge_attrs: Callable to construct later edge_attributes. It receives
+            3 variables (s, p, o) and should construct a dictionary that is
+            passed to networkx's add_edge(s, o, **attrs) function.
+
+            By default this will include setting the 'triples' attribute here,
+            which is treated specially by us to be merged. Other attributes of
+            multi-edges will only contain the attributes of the first edge.
+            If you don't want the 'triples' attribute for tracking, set this to
+            `lambda s, p, o: {}`.
 
     Returns:
         networkx.DiGraph
@@ -117,18 +159,24 @@ def rdflib_to_networkx_digraph(graph,
     3
     >>> dg.size(weight='weight')
     4.0
+
+    >>> dg = rdflib_to_networkx_graph(g, False, edge_attrs=lambda s,p,o:{})
+    >>> 'weight' in dg[a][b]
+    False
+    >>> 'triples' in dg[a][b]
+    False
     """
     import networkx as nx
     dg = nx.DiGraph()
-    _rdflib_to_networkx_graph(graph, dg, calc_weights,
-                              triples_attr, **edge_attrs)
+    _rdflib_to_networkx_graph(graph, dg, calc_weights, edge_attrs, **kwds)
     return dg
 
 
-def rdflib_to_networkx_graph(graph,
-                             calc_weights=True,
-                             triples_attr=True,
-                             **edge_attrs):
+def rdflib_to_networkx_graph(
+        graph,
+        calc_weights=True,
+        edge_attrs=lambda s, p, o: {'triples': [(s, p, o)]},
+        **kwds):
     """Converts the given graph into a networkx.Graph.
 
     As an rdflib.Graph() can contain multiple directed edges between nodes, by
@@ -137,9 +185,17 @@ def rdflib_to_networkx_graph(graph,
     Also by default calculates the edge weight as the len(triples).
 
     Args:
-        calc_weights: If true calculate multi-graph edge-count as edge 'weight'.
-        triples_attr: If true each edge will have a 'triples' attr.
-        **edge_attrs: Attributes added to each edge.
+        graph: a rdflib.Graph.
+        calc_weights: If true calculate multi-graph edge-count as edge 'weight'
+        edge_attrs: Callable to construct later edge_attributes. It receives
+            3 variables (s, p, o) and should construct a dictionary that is
+            passed to networkx's add_edge(s, o, **attrs) function.
+
+            By default this will include setting the 'triples' attribute here,
+            which is treated specially by us to be merged. Other attributes of
+            multi-edges will only contain the attributes of the first edge.
+            If you don't want the 'triples' attribute for tracking, set this to
+            `lambda s, p, o: {}`.
 
     Returns:
         networkx.Graph
@@ -163,11 +219,16 @@ def rdflib_to_networkx_graph(graph,
     2
     >>> ug.size(weight='weight')
     4.0
+
+    >>> ug = rdflib_to_networkx_graph(g, False, edge_attrs=lambda s,p,o:{})
+    >>> 'weight' in ug[a][b]
+    False
+    >>> 'triples' in ug[a][b]
+    False
     """
     import networkx as nx
     g = nx.Graph()
-    _rdflib_to_networkx_graph(graph, g, calc_weights,
-                              triples_attr, **edge_attrs)
+    _rdflib_to_networkx_graph(graph, g, calc_weights, edge_attrs, **kwds)
     return g
 
 
