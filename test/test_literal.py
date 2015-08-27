933fbbded317aec837ecc207c4db54a306375106
Joern Hees <dev@joernhees.de>
2015-08-27 11:45:12 +0200

    dropped test for md5_term_hash as it's deprecated

--- test/test_literal.py
+++ test/test_literal.py
@@ -6,14 +6,6 @@ from rdflib.py3compat import format_doctest_out as uformat
 
 # these are actually meant for test_term.py, which is not yet merged into trunk
 
-class TestMd5(unittest.TestCase):
-    def testMd5(self):
-        self.assertEqual(rdflib.URIRef("http://example.com/").md5_term_hash(),
-                         "40f2c9c20cc0c7716fb576031cceafa4")
-        self.assertEqual(rdflib.Literal("foo").md5_term_hash(),
-                         "da9954ca5f673f8ab9ebd6faf23d1046")
-    
-
 class TestLiteral(unittest.TestCase):
     def setUp(self):
         pass
