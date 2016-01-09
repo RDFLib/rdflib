7fa5b8d9c8a71ca08c1c46de0b1851f5e2a3fe1e
Alessandro Amici <alexamici@gmail.com>
2016-01-10 00:10:01 +0100

    Proposed fix for the broken top_level.txt
    Fixes #571.

--- setup.py
+++ setup.py
@@ -81,22 +81,7 @@ def find_version(filename):
 
 version = find_version('rdflib/__init__.py')
 
-packages = ['rdflib',
-            'rdflib/extras',
-            'rdflib/plugins',
-            'rdflib/plugins/parsers',
-            'rdflib/plugins/parsers/pyRdfa',
-            'rdflib/plugins/parsers/pyRdfa/transform',
-            'rdflib/plugins/parsers/pyRdfa/extras',
-            'rdflib/plugins/parsers/pyRdfa/host',
-            'rdflib/plugins/parsers/pyRdfa/rdfs',
-            'rdflib/plugins/parsers/pyMicrodata',
-            'rdflib/plugins/serializers',
-            'rdflib/plugins/sparql',
-            'rdflib/plugins/sparql/results',
-            'rdflib/plugins/stores',
-            'rdflib/tools'
-              ]
+packages = ['rdflib']
 
 if os.environ.get('READTHEDOCS', None):
     # if building docs for RTD
