7f253518c0c275431a3d1e5002b6f1917c612619
alexisbergeron22 <alexis.bergeron22@yahoo.ca>
2016-11-13 21:57:51 -0500

    Fix date serialization failure

--- rdflib/term.py
+++ rdflib/term.py
@@ -820,7 +820,10 @@ class Literal(Identifier):
                     return self.language > other.language
 
             if self.value != None and other.value != None:
-                return self.value > other.value
+                try:
+                    return self.value > other.value
+                except TypeError:
+                    return str(self.value) > str(other.value)
 
             if unicode(self) != unicode(other):
                 return unicode(self) > unicode(other)
