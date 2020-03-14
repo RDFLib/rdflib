444a43b0f43e40825c1645cafdf96282f097cf89
Kempei Igarashi <kempe@iga-network.com>
2020-03-15 07:24:34 +0900

    Add 'u' to unicode string for py2.7

--- test/test_util.py
+++ test/test_util.py
@@ -236,8 +236,8 @@ class TestUtilTermConvert(unittest.TestCase):
     def test_util_from_n3_expectpartialidempotencewithn3(self):
         for n3 in ('<http://ex.com/foo>',
                    '"foo"@de',
-                   '<http://ex.com/漢字>',
-                   '<http://ex.com/a#あ>',
+                   u'<http://ex.com/漢字>',
+                   u'<http://ex.com/a#あ>',
                    # '"\\""', # exception as '\\"' --> '"' by orig parser as well
                    '"""multi\n"line"\nstring"""@en'):
             self.assertEqual(util.from_n3(n3).n3(), n3,
