9be73860d2ad7dd5e6175e346e0be261a81273d9
Natanael Arndt <arndtn@gmail.com>
2020-03-14 21:03:11 +0100

    Add requirement for requests to setup.py
    The requests package is required by the sparql plugin at several places.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import re
 from setuptools import setup, find_packages
 
 kwargs = {}
-kwargs['install_requires'] = [ 'six', 'isodate', 'pyparsing']
+kwargs['install_requires'] = [ 'six', 'isodate', 'pyparsing', 'requests']
 kwargs['tests_require'] = ['html5lib', 'networkx']
 kwargs['test_suite'] = "nose.collector"
 kwargs['extras_require'] = {'html': ['html5lib']}
