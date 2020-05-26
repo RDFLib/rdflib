2635c8d4295af4ad1a10c569922cc3b3895161d9
ayush17034 <53974855+ayush17034@users.noreply.github.com>
2020-05-27 00:13:45 +0530

    issue 928 fix

--- examples/rdf-json2rdfexample.py
+++ examples/rdf-json2rdfexample.py
@@ -1,7 +1,7 @@
 from rdflib.tools.rdfjson2rdf import getrdffromjson
 
 #replace C:\\Users\hp\PycharmProjects\Sweb\projectrdf.json with your filename
-g = getrdffromjson("C:\\Users\hp\PycharmProjects\Sweb\projectrdf.json")
+g = getrdffromjson("rdf-json2rdfexample.json")
 
 #select any format of rdf serealization
 print(g.serialize(format="turtle").decode("utf-8"))
