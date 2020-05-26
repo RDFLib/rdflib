cc201c13ec531805f948bd3c5253b402907e3fd7
anubhavj99 <anubhav16014@iiitd.ac.in>
2020-05-26 16:13:19 +0530

    Added 'Distinct' functionality to 'Select' function, as kwarg parameter. 1. Moved the functionality of WHERE to another function, to be used in optional and union clauses. 2. Method defined to check instance classes of variables passed in the query.

--- rdflib/QueryBuilder.py
+++ rdflib/QueryBuilder.py
@@ -1,47 +1,62 @@
 from __future__ import unicode_literals
 
-from rdflib import Variable
+from rdflib import Variable, Literal, URIRef
 
 
 class QueryBuilder:
     def __init__(self):
         self.query = ""
         self.SELECT_variables_direct = []
+        self.is_DISTINCT = False
         self.SELECT_variables_with_alias = {}
         self.WHERE_statements = []
 
-    def SELECT(self, *args, **kwargs):
+    @staticmethod
+    def _is_acceptable_query_variable(variable):
+        return isinstance(variable, (Variable, Literal, URIRef))
+
+    def SELECT(self, *args, distinct=False, **kwargs):
+        self.is_DISTINCT = distinct
+
         for var in args:
-            if not isinstance(var, Variable):
-                raise Exception("Argument can be only of type 'Variable'.")
+            if not self._is_acceptable_query_variable(var):
+                raise Exception("Argument not of valid type.")
 
             self.SELECT_variables_direct.append(var)
 
         for var_name, var in kwargs.items():
-            if not isinstance(var, Variable):
-                raise Exception("Argument can be only of type 'Variable'.")
+            if not self._is_acceptable_query_variable(var):
+                raise Exception("Argument not of valid type.")
 
             self.SELECT_variables_with_alias[Variable(var_name)] = var
 
         return self
 
-    def WHERE(self, *args):
+    def process_where_statement(self, append_to_array, *args):
         for statement in args:
             if isinstance(statement, tuple):
                 if len(statement) == 3:
                     s, p, o = statement
-                    if isinstance(s, Variable) and isinstance(p, Variable) and isinstance(o, Variable):
-                        self.WHERE_statements.append((s, p, o))
+                    if self._is_acceptable_query_variable(s) and self._is_acceptable_query_variable(
+                            p) and self._is_acceptable_query_variable(o):
+                        append_to_array.append((s, p, o))
 
                     else:
-                        raise Exception("s, p, o in the statement has to be of type 'Variable'")
+                        raise Exception("s, p, o in the statement not of valid type.")
                 else:
                     raise Exception("Statement has to be a tuple in the format (s, p, o)")
 
+    def WHERE(self, *args, **kwargs):
+        self.process_where_statement(self.WHERE_statements, *args)
+
         return self
 
-    def build(self):
+    def build_select(self):
         self.query += "SELECT "
+
+        if self.is_DISTINCT:
+            self.query += "DISTINCT "
+
         for var in self.SELECT_variables_direct:
             self.query += var.n3() + " "
 
@@ -50,13 +65,18 @@ class QueryBuilder:
 
         self.query += "\n"
 
+    def build_where(self):
         if len(self.WHERE_statements) == 0:
-            raise Exception("Query must have atleast one WHERE statement")
+            raise Exception("Query must have at least one WHERE statement")
 
         self.query += "WHERE {\n"
         for s, p, o in self.WHERE_statements:
             self.query += s.n3() + " " + p.n3() + " " + o.n3() + " ."
         self.query += "\n}"
+
+    def build(self):
+        self.build_select()
+        self.build_where()
 
         return self.query
 
@@ -65,6 +85,7 @@ if __name__ == "__main__":
     query = QueryBuilder().SELECT(
         Variable("s"),
         Variable("p"),
+        distinct=True,
         x=Variable("o")
     ).WHERE(
         (Variable("s"), Variable("p"), Variable("o"))
