c8fb3293b90fb808e5f9a330d48690e351b16322
anubhavj99 <anubhav16014@iiitd.ac.in>
2020-05-28 03:07:37 +0530

    Added functionality for selectin graph for the query being made using FOR_GRAPH. Also added INSERT, DELETE and GROUP BY. 1. acceptable variable type changed to any variable having n3() function 2. IN operator added. 3. CONDITIONAL_STATEMENT updated to handle multiple arguments, such as IN 4. Minor bug in FUNCTION_EXPR multiple parameters n3(). Corrected 5. Example also updated

--- rdflib/query_builder.py
+++ rdflib/query_builder.py
@@ -1,10 +1,11 @@
 from __future__ import unicode_literals
 
-from rdflib import Variable, Literal, URIRef, RDF, OWL, RDFS
+from rdflib import Variable, Literal, URIRef
+from rdflib.namespace import RDFS, OWL, RDF, XSD
 
 
 def is_acceptable_query_variable(variable):
-    return isinstance(variable, (CONDITIONAL_STATEMENT, FILTER, FUNCTION_EXPR, AGGREGATE, Variable, Literal, URIRef))
+    return hasattr(variable, "n3")
 
 
 class STATEMENT(tuple):
@@ -85,6 +86,16 @@ class Operators(object):
         else:
             raise Exception("Operands are not of acceptable type.")
 
+    @staticmethod
+    def IN(left, *args):
+        if not is_acceptable_query_variable(left):
+            raise Exception("Operands are not of acceptable type.")
+        for var in args:
+            if not is_acceptable_query_variable(var):
+                raise Exception("Operands are not of acceptable type.")
+
+        return CONDITIONAL_STATEMENT(left, "IN", *args)
+
 
 class OPTIONAL(STATEMENT):
     def __new__(cls, statement):
@@ -96,11 +107,22 @@ class OPTIONAL(STATEMENT):
 
 
 class CONDITIONAL_STATEMENT(STATEMENT):
-    def __new__(cls, left, operator, right):
-        return tuple.__new__(CONDITIONAL_STATEMENT, (left, operator, right))
+    def __new__(cls, left, operator, *args):
+        return tuple.__new__(CONDITIONAL_STATEMENT, (left, operator, args))
 
     def n3(self):
-        return self[0].n3() + " " + self[1] + " " + self[2].n3()
+        n3_string = self[0].n3() + " " + self[1] + " "
+        if len(self[2]) == 1:
+            n3_string += self[2][0].n3()
+        else:
+            n3_string += "( "
+            for i in range(len(self[2])):
+                n3_string += self[2][i].n3()
+                if i < len(self[2]) - 1:
+                    n3_string += ", "
+            n3_string += " )"
+
+        return n3_string
 
 
 list_function = ["SUM", "AVG", "COUNT", "SET", "MIN", "MAX", "GROUPCONTACT", "SAMPLE"]
@@ -128,15 +150,14 @@ class AGGREGATE(STATEMENT):
 
 
 def create_aggregate(function):
-
     def new(cls, statement):
         return AGGREGATE.__new__(cls, function, statement)
+
     return type(str(function), (AGGREGATE,), dict(__new__=new))
 
 
 for function in list_function:
     setattr(AGGREGATE, function, create_aggregate(function))
-
 
 supported_function_expression_list = [
     "ASC", "DESC", "IRI", "ISBLANK", "ISLITERAL", "ISIRI", "ISNUMERIC", "BNODE",
@@ -172,22 +193,24 @@ class FUNCTION_EXPR(STATEMENT):
         return tuple.__new__(FUNCTION_EXPR, (function_expression, args))
 
     def n3(self):
-        n3_string = self[0] + "( "
-        for var in self[1]:
-            n3_string += var.n3() + " "
-        n3_string += ")"
+        n3_string = self[0] + " ( "
+        for i in range(len(self[1])):
+            n3_string += self[1][i].n3()
+            if i < len(self[1]) - 1:
+                n3_string += ", "
+        n3_string += " )"
         return n3_string
 
 
 def create_function_expr(function_expression):
     def new(cls, *args):
         return FUNCTION_EXPR.__new__(cls, function_expression, *args)
+
     return type(str(function_expression), (FUNCTION_EXPR,), dict(__new__=new))
 
 
 for function_expression in supported_function_expression_list:
     setattr(FUNCTION_EXPR, function_expression, create_function_expr(function_expression))
-
 
 
 class FILTER(STATEMENT):
@@ -205,8 +228,44 @@ class FILTER(STATEMENT):
         return "FILTER ( " + self[0].n3() + " )"
 
 
-class QueryBuilder:
+class FOR_GRAPH(STATEMENT):
+    def __new__(cls, *args, name=None, **kwargs):
+        if name and not is_acceptable_query_variable(name):
+            raise Exception("GRAPH name not of acceptable type.")
+        for statement in args:
+            if not is_acceptable_query_variable(statement):
+                raise Exception(
+                    "Statement {} for GRAPH {} not of acceptable type.".format(
+                        statement, name
+                    )
+                )
+        args_with_alias = {}
+        for alias, statement in kwargs.items():
+            if is_acceptable_query_variable(statement):
+                args_with_alias[Variable(alias)] = statement
+            else:
+                raise Exception(
+                    "Statement {} for GRAPH {} not of acceptable type.".format(
+                        statement, name
+                    )
+                )
+        return tuple.__new__(FOR_GRAPH, (name, args, args_with_alias))
 
+    def n3(self):
+        n3_string = ""
+        if self[0]:
+            n3_string += "GRAPH " + self[0].n3() + " "
+        n3_string += "{ \n"
+        for var in self[1]:
+            n3_string += var.n3() + " "
+        for alias, var in self[2].items():
+            n3_string += var.n3() + " as " + alias.n3() + " "
+
+        n3_string += "\n} "
+        return n3_string
+
+
+class QueryBuilder:
     class QUERY_STRING(str):
         def __new__(cls, query):
             return str.__new__(cls, query)
@@ -216,10 +275,14 @@ class QueryBuilder:
 
     def __init__(self):
         self.query = ""
-        self.SELECT_variables_direct = []
         self.is_DISTINCT = False
+        self.SELECT_variables_direct = []
         self.SELECT_variables_with_alias = {}
+        self.INSERT_variables_direct = []
+        self.INSERT_variables_with_alias = {}
+        self.DELETE_variables_direct = []
         self.WHERE_statements = []
+        self.GROUP_BY_expressions = []
         self.ORDER_BY_expressions = []
 
         self.limit = None
@@ -248,6 +311,36 @@ class QueryBuilder:
 
         return self
 
+    def INSERT(self, *args, **kwargs):
+        for var in args:
+            if isinstance(var, AGGREGATE):
+                raise Exception(
+                    "Alias not provided for {}".format(
+                        var[1].n3()
+                    )
+                )
+            if not is_acceptable_query_variable(var):
+                raise Exception("Argument not of valid type.")
+
+            self.INSERT_variables_direct.append(var)
+
+        for var_name, var in kwargs.items():
+            if not is_acceptable_query_variable(var):
+                raise Exception("Argument not of valid type.")
+
+            self.INSERT_variables_with_alias[Variable(var_name)] = var
+
+        return self
+
+    def DELETE(self, *args):
+        for var in args:
+            if not is_acceptable_query_variable(var):
+                raise Exception("Argument not of valid type.")
+
+            self.DELETE_variables_direct.append(var)
+
+        return self
+
     def WHERE(self, *args, **kwargs):
         for statement in args:
             if not hasattr(statement, "n3"):
@@ -267,6 +360,15 @@ class QueryBuilder:
 
         return self
 
+    def GROUP_BY(self, *args):
+        for var in args:
+            if is_acceptable_query_variable(var):
+                self.GROUP_BY_expressions.append(var)
+            else:
+                raise Exception("Expression passed in ORDER_BY is not valid.")
+
+        return self
+
     def ORDER_BY(self, *args):
         for var in args:
             if is_acceptable_query_variable(var):
@@ -277,18 +379,40 @@ class QueryBuilder:
         return self
 
     def build_select(self):
-        self.query += "SELECT "
+        if len(self.SELECT_variables_direct) + len(self.SELECT_variables_with_alias) > 0:
+            self.query += "SELECT "
 
-        if self.is_DISTINCT:
-            self.query += "DISTINCT "
+            if self.is_DISTINCT:
+                self.query += "DISTINCT "
 
-        for var in self.SELECT_variables_direct:
-            self.query += var.n3() + " "
+            for var in self.SELECT_variables_direct:
+                self.query += var.n3() + " "
 
-        for var_alias, var_expression in self.SELECT_variables_with_alias.items():
-            self.query += "(" + var_expression.n3() + " as " + var_alias.n3() + ") "
+            for var_alias, var_expression in self.SELECT_variables_with_alias.items():
+                self.query += "(" + var_expression.n3() + " as " + var_alias.n3() + ") "
 
-        self.query += " \n"
+            self.query += " \n"
+
+    def build_insert(self):
+        if len(self.INSERT_variables_direct) + len(self.INSERT_variables_with_alias) > 0:
+            self.query += "INSERT { \n"
+
+            for var in self.INSERT_variables_direct:
+                self.query += var.n3() + " "
+
+            for var_alias, var_expression in self.INSERT_variables_with_alias.items():
+                self.query += "(" + var_expression.n3() + " as " + var_alias.n3() + ") "
+
+            self.query += "\n} \n"
+
+    def build_delete(self):
+        if len(self.DELETE_variables_direct) > 0:
+            self.query += "DELETE { \n"
+
+            for var in self.DELETE_variables_direct:
+                self.query += var.n3() + " "
+
+            self.query += "\n} \n"
 
     def build_where(self):
         if len(self.WHERE_statements) == 0:
@@ -301,7 +425,13 @@ class QueryBuilder:
 
         self.query += "}" + " \n"
 
-    def build_order_by(self):
+    def build_group_by_order_by(self):
+        if len(self.GROUP_BY_expressions) > 0:
+            self.query += "GROUP BY "
+            for var in self.GROUP_BY_expressions:
+                self.query += var.n3() + " "
+            self.query += "\n"
+
         if len(self.ORDER_BY_expressions) > 0:
             self.query += "ORDER BY "
             for var in self.ORDER_BY_expressions:
@@ -317,35 +447,51 @@ class QueryBuilder:
 
     def build(self):
         self.build_select()
+        self.build_insert()
+        self.build_delete()
         self.build_where()
 
-        self.build_order_by()
+        self.build_group_by_order_by()
         self.build_limit_offset()
 
         return QueryBuilder.QUERY_STRING(self.query)
 
 
 if __name__ == "__main__":
-
-    query = QueryBuilder().SELECT(
-        Variable("s"),
-        Variable("p"),
-        x=Variable("o"),
-        value=AGGREGATE.AVG(Variable("v")),
-        sum_value=AGGREGATE.SUM(Variable("v")),
-        distinct=True
+    query = QueryBuilder().INSERT(
+        FOR_GRAPH(
+            Variable("s"),
+            Variable("p"),
+            Variable("o"),
+            name=URIRef("graph_name_1")
+        )
     ).WHERE(
         (Variable("s"), Variable("p"), Variable("o")),
         (Variable("o"), RDF.type, Variable("v")),
         OPTIONAL(
             (Variable("o"), RDFS.subClassOf, OWL.thing)
         ),
-        FILTER(
-            Operators.AND(
-                Operators.GE(Variable("v"), Literal(5)),
-                Operators.LT(Variable("v"), Literal(13))
+        FOR_GRAPH(
+            FILTER(
+                Operators.AND(
+                    Operators.GE(Variable("v"), Literal(5)),
+                    Operators.LT(Variable("v"), Literal(13))
+                )
+            ),
+            name=URIRef("arshgraph_name_2")
+        ),
+        FOR_GRAPH(
+            FILTER(
+                Operators.IN(
+                    Variable("v"),
+                    Literal("literal_string_1"),
+                    Literal("12", datatype=XSD.integer)
+                )
             )
         )
+    ).GROUP_BY(
+        Variable("v"),
+        Variable("s")
     ).ORDER_BY(
         Variable("v"),
         FUNCTION_EXPR.ASC(Variable("s"))
@@ -354,4 +500,4 @@ if __name__ == "__main__":
     ).OFFSET(
         20
     ).build()
-    print(query.n3())
+    print(query)
