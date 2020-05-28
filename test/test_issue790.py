b883ec2963203a83ad6eaf57a641fc4be69856e1
saksham16085 <saksham16085@iiitd.ac.in>
2020-05-28 17:13:01 +0530

    Test cases added for query builder in test_issue790.py. 1. OR operator, exceptions for insert delete, insert, group by, order by, for graph. 2. Exceptions also added for the same.

--- test/test_issue790.py
+++ test/test_issue790.py
@@ -8,11 +8,15 @@ class TestQueryBuilder_Issue790(unittest.TestCase):
     def setUp(self):
         self.var_s = Variable("s")
         self.var_p = Variable("p")
+        self.var_p2 = Variable("p2")
         self.var_o = Variable("o")
         self.var_v = Variable("v")
         self.var_unacceptable = "s"
         self.literal_5 = Literal(5)
         self.literal_13 = Literal(13)
+        self.literal_20 = Literal(20)
+        self.literal_30 = Literal(30)
+
         self.insert_query = "INSERT { ?s ?p ?o } WHERE { ?s ?p ?o . } "
         self.operator_query_gt = "SELECT ?v  WHERE { ?s ?p ?v . FILTER ( ?v > " \
                                  "\"5\"^^<http://www.w3.org/2001/XMLSchema#integer> ) . } "
@@ -29,13 +33,21 @@ class TestQueryBuilder_Issue790(unittest.TestCase):
         self.operator_query_in = "SELECT ?v  WHERE { ?s ?p ?v . FILTER ( " \
                                  "\"5\"^^<http://www.w3.org/2001/XMLSchema#integer> IN ( ?v, " \
                                  "\"5\"^^<http://www.w3.org/2001/XMLSchema#integer> ) ) . } "
+        self.group_by_query = "SELECT ?o  WHERE { ?s ?p ?o . } GROUP BY ?o "
+        self.delete_query = "DELETE { ?s ?p ?o } WHERE { ?s ?p ?o . ?o ?p2 ?v . } "
+        self.for_graph_query = "SELECT ?s ?p ?o  WHERE { ?s ?p ?o . GRAPH <graph_name_1> { ?o ?p2 ?v . OPTI" \
+                               "ONAL { ?s ?p ?o . } . }  . } "
+        self.nested_query = "SELECT ?s ?p ?o  WHERE { FILTER ( ?o IN {SELECT ?s  WHERE { ?s ?p2 ?o . } } ) . } "
+        self.function_expr_multiple_parameters = "SELECT ?s  WHERE { ?s ?p ?o . FILTER ( CONTAINS ( LCASE ( ?o " \
+                                                 "), \"hey\" ) ) . } "
         self.basic_query = "SELECT ?s (?o as ?x)  WHERE { ?s ?p ?o . } "
-        self.full_query = "SELECT DISTINCT ?s ?p (?o as ?x) (AVG( ?v ) as ?value) (SUM( ?v ) as ?sum_value)" \
-                          "  WHERE { ?s ?p ?o . ?o <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> ?v . O" \
-                          "PTIONAL { ?o <http://www.w3.org/2000/01/rdf-schema#subClassOf> <http://www.w3.or" \
-                          "g/2002/07/owl#thing> } . FILTER ( ?v >= \"5\"^^<http://www.w3.org/2001/XMLSchema" \
-                          "#integer> && ?v < \"13\"^^<http://www.w3.org/2001/XMLSchema#integer> ) . } ORDE" \
-                          "R BY ?v ASC ( ?s ) LIMIT 100 OFFSET 20 "
+        self.full_query = "SELECT DISTINCT ?s ?p (?o as ?x) (AVG( ?v ) as ?value) (SUM( ?v ) as ?sum_value)  " \
+                          "WHERE { ?s ?p ?o . ?o <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> ?v . OPTIO" \
+                          "NAL { ?o <http://www.w3.org/2000/01/rdf-schema#subClassOf> <http://www.w3.org/2002" \
+                          "/07/owl#thing> . } . FILTER ( ?v >= \"5\"^^<http://www.w3.org/2001/XMLSchema#integ" \
+                          "er> && ?v < \"13\"^^<http://www.w3.org/2001/XMLSchema#integer> || ?v >= \"20\"^^<h" \
+                          "ttp://www.w3.org/2001/XMLSchema#integer> && ?v < \"30\"^^<http://www.w3.org/2001/X" \
+                          "MLSchema#integer> ) . } ORDER BY ?v ASC ( ?s ) LIMIT 100 OFFSET 20 "
 
     def test_full_query_should_pass(self):
         query = QueryBuilder().SELECT(
@@ -52,9 +64,15 @@ class TestQueryBuilder_Issue790(unittest.TestCase):
                 (self.var_o, RDFS.subClassOf, OWL.thing)
             ),
             FILTER(
-                Operators.AND(
-                    Operators.GE(self.var_v, self.literal_5),
-                    Operators.LT(self.var_v, self.literal_13)
+                Operators.OR(
+                    Operators.AND(
+                        Operators.GE(self.var_v, self.literal_5),
+                        Operators.LT(self.var_v, self.literal_13)
+                    ),
+                    Operators.AND(
+                        Operators.GE(self.var_v, self.literal_20),
+                        Operators.LT(self.var_v, self.literal_30)
+                    )
                 )
             )
         ).ORDER_BY(
@@ -77,15 +95,25 @@ class TestQueryBuilder_Issue790(unittest.TestCase):
         self.assertEqual(self.basic_query, query.replace("\n", ""),
                          msg="QueryBuilder not returning correct basic query.")
 
-    def test_query_insert(self):
+    def test_query_insert_should_pass(self):
         query = QueryBuilder().INSERT(
-                self.var_s,
-                self.var_p,
-                self.var_o,
-            ).WHERE(
-                (self.var_s, self.var_p, self.var_o)
+            self.var_s,
+            self.var_p,
+            self.var_o,
+        ).WHERE(
+            (self.var_s, self.var_p, self.var_o)
         ).build()
         self.assertEqual(self.insert_query, query.replace("\n", ""), msg="QueryBuilder not performing insert correctly")
+
+    def test_query_insert_with_incorrect_parameter_raises_Exception(self):
+        with self.assertRaises(Exception):
+            QueryBuilder().INSERT(
+                self.var_unacceptable,
+                self.var_p,
+                self.var_o
+            ).WHERE(
+                (self.var_s, self.var_p, self.var_o)
+            ).build()
 
     def test_query_operators_should_pass(self):
         query = QueryBuilder().SELECT(
@@ -96,8 +124,8 @@ class TestQueryBuilder_Issue790(unittest.TestCase):
                 Operators.GT(self.var_v, self.literal_5)
             )
         ).build()
-        self.assertEqual(self.operator_query_gt, query.replace("\n", ""), msg="QueryBuilder not returning correct "
-                                                                           "operator GT query.")
+        self.assertEqual(self.operator_query_gt, query.replace("\n", ""),
+                         msg="QueryBuilder not returning correct operator GT query.")
 
         query = QueryBuilder().SELECT(
             self.var_v
@@ -107,8 +135,8 @@ class TestQueryBuilder_Issue790(unittest.TestCase):
                 Operators.LT(self.var_v, self.literal_5)
             )
         ).build()
-        self.assertEqual(self.operator_query_lt, query.replace("\n", ""), msg="QueryBuilder not returning correct "
-                                                                           "operator LT query.")
+        self.assertEqual(self.operator_query_lt, query.replace("\n", ""),
+                         msg="QueryBuilder not returning correct operator LT query.")
 
         query = QueryBuilder().SELECT(
             self.var_v
@@ -118,8 +146,8 @@ class TestQueryBuilder_Issue790(unittest.TestCase):
                 Operators.EQ(self.var_v, self.literal_5)
             )
         ).build()
-        self.assertEqual(self.operator_query_eq, query.replace("\n", ""), msg="QueryBuilder not returning correct "
-                                                                           "operator EQ query.")
+        self.assertEqual(self.operator_query_eq, query.replace("\n", ""),
+                         msg="QueryBuilder not returning correct operator EQ query.")
 
         query = QueryBuilder().SELECT(
             self.var_v
@@ -129,8 +157,8 @@ class TestQueryBuilder_Issue790(unittest.TestCase):
                 Operators.NE(self.var_v, self.literal_5)
             )
         ).build()
-        self.assertEqual(self.operator_query_ne, query.replace("\n", ""), msg="QueryBuilder not returning correct "
-                                                                           "operator NE query.")
+        self.assertEqual(self.operator_query_ne, query.replace("\n", ""),
+                         msg="QueryBuilder not returning correct operator NE query.")
 
         query = QueryBuilder().SELECT(
             self.var_v
@@ -140,8 +168,8 @@ class TestQueryBuilder_Issue790(unittest.TestCase):
                 Operators.GE(self.var_v, self.literal_5)
             )
         ).build()
-        self.assertEqual(self.operator_query_ge, query.replace("\n", ""), msg="QueryBuilder not returning correct "
-                                                                           "operator GE query.")
+        self.assertEqual(self.operator_query_ge, query.replace("\n", ""),
+                         msg="QueryBuilder not returning correct operator GE query.")
 
         query = QueryBuilder().SELECT(
             self.var_v
@@ -151,8 +179,8 @@ class TestQueryBuilder_Issue790(unittest.TestCase):
                 Operators.LE(self.var_v, self.literal_5)
             )
         ).build()
-        self.assertEqual(self.operator_query_le, query.replace("\n", ""), msg="QueryBuilder not returning correct "
-                                                                           "operator LE query.")
+        self.assertEqual(self.operator_query_le, query.replace("\n", ""),
+                         msg="QueryBuilder not returning correct operator LE query.")
 
         query = QueryBuilder().SELECT(
             self.var_v
@@ -162,11 +190,10 @@ class TestQueryBuilder_Issue790(unittest.TestCase):
                 Operators.IN(self.literal_5, self.var_v, self.literal_5)
             )
         ).build()
-        self.assertEqual(self.operator_query_in, query.replace("\n", ""), msg="QueryBuilder not returning correct "
-                                                                              "operator IN query.")
+        self.assertEqual(self.operator_query_in, query.replace("\n", ""),
+                         msg="QueryBuilder not returning correct operator IN query.")
 
     def test_query_with_incorrect_expression_in_operators_raises_Exception(self):
-
         with self.assertRaises(Exception):
             QueryBuilder().SELECT(
                 self.var_s
@@ -361,7 +388,6 @@ class TestQueryBuilder_Issue790(unittest.TestCase):
                 )
             ).build()
 
-
     def test_query_without_where_raises_Exception(self):
         with self.assertRaises(Exception):
             QueryBuilder().SELECT(self.var_s).build()
@@ -478,3 +504,111 @@ class TestQueryBuilder_Issue790(unittest.TestCase):
             ).WHERE(
                 (self.var_s, self.var_p, self.var_o),
             ).build()
+
+    def test_query_with_group_by_should_pass(self):
+        query = QueryBuilder().SELECT(
+            self.var_o
+        ).WHERE(
+            (self.var_s, self.var_p, self.var_o)
+        ).GROUP_BY(
+            self.var_o
+        ).build()
+        self.assertEqual(self.group_by_query, query.replace("\n", ""),
+                         msg="QueryBuilder not returning correct group by query.")
+
+    def test_query_with_group_by_incorrect_parameter_raises_Exception(self):
+        with self.assertRaises(Exception):
+            QueryBuilder().SELECT(
+                self.var_o
+            ).WHERE(
+                (self.var_s, self.var_p, self.var_o)
+            ).GROUP_BY(
+                self.var_unacceptable
+            ).build()
+
+    def test_query_with_delete_should_pass(self):
+        query = QueryBuilder().DELETE(
+            self.var_s, self.var_p, self.var_o
+        ).WHERE(
+            (self.var_s, self.var_p, self.var_o),
+            (self.var_o, self.var_p2, self.var_v)
+        ).build()
+        self.assertEqual(self.delete_query, query.replace("\n", ""),
+                         msg="QueryBuilder not returning correct delete query.")
+
+    def test_query_with_delete_incorrect_parameter_raises_Exception(self):
+        with self.assertRaises(Exception):
+            QueryBuilder().DELETE(
+                self.var_s, self.var_p, self.var_unacceptable
+            ).WHERE(
+                (self.var_s, self.var_p, self.var_o),
+                (self.var_o, self.var_p2, self.var_v)
+            ).build()
+
+    def test_query_with_order_by_incorrect_parameter_raises_Exception(self):
+        with self.assertRaises(Exception):
+            QueryBuilder().SELECT(
+                self.var_s
+            ).WHERE(
+                (self.var_s, self.var_p, self.var_o)
+            ).GROUP_BY(
+                self.var_s
+            ).ORDER_BY(
+                self.var_unacceptable
+            ).build()
+
+    def test_query_for_graph_should_pass(self):
+        query = QueryBuilder().SELECT(
+            self.var_s, self.var_p, self.var_o
+        ).WHERE(
+            (self.var_s, self.var_p, self.var_o),
+            FOR_GRAPH(
+                (self.var_o, self.var_p2, self.var_v),
+                OPTIONAL(
+                    (self.var_s, self.var_p, self.var_o)
+                ),
+                name=URIRef("graph_name_1")
+            )
+        ).build()
+        self.assertEqual(self.for_graph_query, query.replace("\n", ""),
+                         msg="QueryBuilder not returning correct query for FOR_GRAPH functions.")
+
+    def test_query_for_graph_name_unacceptable_raises_Exception(self):
+        with self.assertRaises(Exception):
+            QueryBuilder().SELECT(
+                self.var_s, self.var_p, self.var_o
+            ).WHERE(
+                (self.var_s, self.var_p, self.var_o),
+                FOR_GRAPH(
+                    (self.var_o, self.var_p2, self.var_v),
+                    name="graph_name_1"
+                )
+            ).build()
+
+    def test_query_with_nested_query_should_pass(self):
+        query = QueryBuilder().SELECT(
+            self.var_s, self.var_p, self.var_o
+        ).WHERE(
+            FILTER(
+                Operators.IN(
+                    self.var_o,
+                    QueryBuilder().SELECT(
+                        self.var_s
+                    ).WHERE(
+                        (self.var_s, self.var_p2, self.var_o)
+                    ).build()
+                )
+            )
+        ).build()
+        self.assertEqual(self.nested_query, query.replace("\n", ""),
+                         msg="QueryBuilder not returning correct query for nested queries.")
+
+    def test_query_function_expr_with_multiple_paramters_should_pass(self):
+        query = QueryBuilder().SELECT(
+            self.var_s
+        ).WHERE(
+            (self.var_s, self.var_p, self.var_o),
+            FILTER(FUNCTION_EXPR.CONTAINS(FUNCTION_EXPR.LCASE(self.var_o), Literal("hey")))
+        ).build()
+        self.assertEqual(self.function_expr_multiple_parameters, query.replace("\n", ""),
+                         msg="QueryBuilder not returning correct query for function expr with multiple parameters.")
