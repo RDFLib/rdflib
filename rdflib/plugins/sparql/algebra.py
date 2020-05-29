bab4ee9b9489cf1cd210b8bde1b03fa58c6c9c48
devrb <Ritam.Biswas2020@gmail.com>
2020-05-29 07:35:19 +0530

    Fix issue #936
    Modified the algebra.py file under sparql folder

--- rdflib/plugins/sparql/algebra.py
+++ rdflib/plugins/sparql/algebra.py
@@ -498,7 +498,7 @@ def translateAggregates(q, M):
                 v.expr = traverse(v.expr, functools.partial(_aggs, A=A))
 
     # having clause
-    if traverse(q.having, _hasAggregate, complete=False):
+    if traverse(q.having, _hasAggregate, complete=True):
         q.having = traverse(q.having, _sample)
         traverse(q.having, functools.partial(_aggs, A=A))
 
