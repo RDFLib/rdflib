6a02e02f09dddb6c9bc1dc4c5c1fac7b75864fa5
Donny Winston <dwinston@alum.mit.edu>
2020-06-12 16:50:37 -0400

    make csv2rdf work in py3; add test

---
+++ test/test_csv2rdf.py
@@ -0,0 +1,15 @@
+import subprocess
+import unittest
+
+class CSV2RDFTest(unittest.TestCase):
+    def test_csv2rdf_cli(self):
+        completed = subprocess.run(
+            ["csv2rdf", "test/csv/realestate.csv"],
+            capture_output=True,
+            text=True,
+        )
+        self.assertEqual(completed.returncode, 0)
+        self.assertRegex(completed.stderr, r"Converted \d+ rows into \d+ triples.")
+        self.assertRegex(completed.stderr, r"Took [\d\.]+ seconds.")
+        self.assertIn("<http://example.org/instances/0>", completed.stdout)
+        self.assertIn("<http://example.org/props/zip>", completed.stdout)
