c50ee269c3a518e72931e476dd592ec5336b679a
Ashley Sommer <ashleysommer@gmail.com>
2020-09-18 00:26:51 +1000

    add a missed Black file

--- rdflib/namespace.py
+++ rdflib/namespace.py
@@ -212,10 +212,11 @@ class ClosedNamespace(object):
 
     def __dir__(self):
         return list(self._ClosedNamespace__uris)
-    
+
     def _ipython_key_completions_(self):
         return dir(self)
-    
+
+
 class _RDFNamespace(ClosedNamespace):
     """
     Closed namespace for RDF terms
