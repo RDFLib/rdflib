33271934ade16f11ba0bcd0f5b373216a4bb7bb8
Natanael Arndt <arndtn@gmail.com>
2021-03-22 11:50:44 +0100

    use PEP440 version string

--- rdflib/__init__.py
+++ rdflib/__init__.py
@@ -45,7 +45,7 @@ A tiny example:
 __docformat__ = "restructuredtext en"
 
 # The format of the __version__ line is matched by a regex in setup.py
-__version__ = "6.0.0-pre-alpha"
+__version__ = "6.0.0-alpha"
 __date__ = "2021-03-22"
 
 __all__ = [
