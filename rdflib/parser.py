db9ebf4f92e87a738052ef698536dbf35bdd8cc2
Iwan Aucamp <aucampia@gmail.com>
2021-05-13 23:05:29 +0200

    Add pathlib.PurePath support for Graph.{serialize,parse}
    Graph.parse did already support pathlib.Path but there is no good
    reason to not support PurePath AFAICT.

--- rdflib/parser.py
+++ rdflib/parser.py
@@ -238,7 +238,7 @@ def create_input_source(
         else:
             if isinstance(source, str):
                 location = source
-            elif isinstance(source, pathlib.Path):
+            elif isinstance(source, pathlib.PurePath):
                 location = str(source)
             elif isinstance(source, bytes):
                 data = source
