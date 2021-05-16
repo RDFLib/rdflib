388e47258c14adbf796172e61be629f0f5c34709
Nicholas Car <nicholas.car@surroundaustralia.com>
2021-05-17 09:44:52 +1000

    remove decode() causing failure
    This test was created before the need to decode graph serialization results was removed, so just updating for that. Now it should pass

--- test/test_issue977.py
+++ test/test_issue977.py
@@ -11,15 +11,15 @@ class TestIssue977(unittest.TestCase):
         self.graph.bind('isbn', 'urn:isbn:')
         self.graph.bind('webn', 'http://w3c.org/example/isbn/')
         # Populate graph.
-        self.graph.add((URIRef('urn:isbn:1503280780'), RDFS.label, Literal('Moby Dick') ))
-        self.graph.add((URIRef('http://w3c.org/example/isbn/1503280780'), RDFS.label, Literal('Moby Dick') ))        
+        self.graph.add((URIRef('urn:isbn:1503280780'), RDFS.label, Literal('Moby Dick')))
+        self.graph.add((URIRef('http://w3c.org/example/isbn/1503280780'), RDFS.label, Literal('Moby Dick')))        
 
     def test_namespace_manager(self):
         assert 'isbn', 'urn:isbn:' in tuple(self.graph.namespaces())
         assert 'webn', 'http://w3c.org/example/isbn/' in tuple(self.graph.namespaces())
 
     def test_turtle_serialization(self):
-        serialization = self.graph.serialize(None, format='turtle').decode('utf-8')
+        serialization = self.graph.serialize(None, format='turtle')
         print(f'Test Issue 977, serialization output:\n---\n{serialization}---')
         # Test serialization.
         assert '@prefix webn:' in serialization, "Prefix webn not found in serialization!"
