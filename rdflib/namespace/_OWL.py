f460a72c219739b5482a744146ffc325f72a9362
Nicholas Car <nicholas.car@surroundaustralia.com>
2021-10-10 12:29:14 +1000

    added definition location of OWL datatypes

--- rdflib/namespace/_OWL.py
+++ rdflib/namespace/_OWL.py
@@ -116,6 +116,8 @@ class OWL(DefinedNamespace):
     versionIRI: URIRef  # The property that identifies the version IRI of an ontology.
 
     # http://www.w3.org/2000/01/rdf-schema#Datatype
+    # NOTE: the following two elements don't appear in the OWL RDF documents but are defined in the OWL2 Recommentation
+    # at https://www.w3.org/TR/owl2-syntax/#Datatype_Maps
     rational: URIRef  # The value space is the set of all rational numbers. The lexical form is numerator '/' denominator, where both are integers.
     real: URIRef  # The value space is the set of all real numbers. Does not directly provide any lexical forms.
 
