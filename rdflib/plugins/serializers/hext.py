64e3e311fe0550e62573713eb9eeb86b3f799dcb
nicholascar <nicholas.car@surroundaustralia.com>
2021-12-03 22:06:34 +1000

    ensure both context aware and not work

--- rdflib/plugins/serializers/hext.py
+++ rdflib/plugins/serializers/hext.py
@@ -2,10 +2,9 @@
 HextuplesSerializer RDF graph serializer for RDFLib.
 See <https://github.com/ontola/hextuples> for details about the format.
 """
-from typing import IO, Optional
-
-from rdflib.graph import Graph
-from rdflib.term import Literal, URIRef, BNode
+from typing import IO, TYPE_CHECKING, Optional, Union
+from rdflib.graph import Graph, ConjunctiveGraph
+from rdflib.term import Literal, URIRef, Node
 from rdflib.serializer import Serializer
 
 __all__ = ["HextuplesSerializer"]
@@ -16,7 +15,19 @@ class HextuplesSerializer(Serializer):
     Serializes RDF graphs to NTriples format.
     """
 
-    def __init__(self, store: Graph):
+    def __init__(self, store: Union[Graph, ConjunctiveGraph]):
+        self.default_context: Optional[Node]
+        if store.context_aware:
+            if TYPE_CHECKING:
+                assert isinstance(store, ConjunctiveGraph)
+            self.contexts = list(store.contexts())
+            self.default_context = store.default_context.identifier
+            if store.default_context:
+                self.contexts.append(store.default_context)
+        else:
+            self.contexts = [store]
+            self.default_context = None
+
         Serializer.__init__(self, store)
         self.encoding = "utf-8"
 
@@ -28,7 +39,7 @@ class HextuplesSerializer(Serializer):
         **args
     ):
         self.encoding = encoding
-        for context in self.store.contexts():
+        for context in self.contexts:
             for triple in context:
                 stream.write(
                     _hex_line(triple, context.identifier).encode(self.encoding)
--- test/test_serialize_hext.py
+++ test/test_serialize_hext.py
@@ -1,14 +1,38 @@
-from rdflib import Dataset, URIRef, Namespace, Literal, BNode
-from test import TEST_DIR
+from rdflib import Dataset, Graph
 
 
-def test_hext_01():
-    d = Dataset()
-    trig = """
+def test_hext_graph():
+    g = Graph()
+    turtle_data = """
             PREFIX ex: <http://example.com/>
             PREFIX owl: <http://www.w3.org/2002/07/owl#>
             PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-            PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+            PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+
+            ex:s1
+                ex:p1 ex:o1 , ex:o2 ;
+                ex:p2 [
+                    a owl:Thing ;
+                    rdf:value "thingy" ;
+                ] ;
+                ex:p3 "Object 3" , "Object 4 - English"@en ;
+                ex:p4 "2021-12-03"^^xsd:date ;
+                ex:p5 42 ;
+                ex:p6 "42" ;
+            .
+            """
+
+    g.parse(data=turtle_data, format="turtle")
+    out = g.serialize(format="hext")
+    print(out)
+
+
+def test_hext_dataset():
+    d = Dataset()
+    trig_data = """
+            PREFIX ex: <http://example.com/>
+            PREFIX owl: <http://www.w3.org/2002/07/owl#>
+            PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
             PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
 
             ex:g1 {
@@ -37,7 +61,7 @@ def test_hext_01():
             ex:s21 ex:p21 ex:o21 , ex:o22 .
 
            """
-    d.parse(data=trig, format="trig")
+    d.parse(data=trig_data, format="trig")
     out = d.serialize(format="hext")
     testing_lines = [
         [False, '["http://example.com/s1", "http://example.com/p1", "http://example.com/o2", "", "", "http://example.com/g2"]'],
