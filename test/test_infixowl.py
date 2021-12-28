29f67cd290c1e05604adefce34815e3599aeed72
Graham Higgins <gjh@bel-epa.com>
2021-12-28 11:36:14 +0000

    changes per comments

--- test/test_infixowl.py
+++ test/test_infixowl.py
@@ -1,4 +1,4 @@
-from rdflib import logger, BNode, Graph, Literal, Namespace, RDFS, OWL, XSD
+from rdflib import BNode, Graph, Literal, Namespace, RDFS, OWL, XSD
 from rdflib.namespace import NamespaceManager
 from rdflib.util import first
 from rdflib.extras.infixowl import (
@@ -15,7 +15,6 @@ from rdflib.extras.infixowl import (
 )
 
 
-# @pytest.mark.skip
 def test_infix_owl_example1():
 
     exNs = Namespace("http://example.com/")
@@ -30,7 +29,7 @@ def test_infix_owl_example1():
     classD = Class(exNs.D)
     anonClass = exNs.someProp << some >> classD
 
-    assert repr(anonClass) == "( ex:someProp SOME ex:D )"
+    assert str(anonClass) == "( ex:someProp SOME ex:D )"
 
     a = Class(exNs.Opera, graph=g)
 
@@ -43,7 +42,7 @@ def test_infix_owl_example1():
 
     # We can then access the rdfs:subClassOf relationships
 
-    assert repr(list(a.subClassOf)) == "[Class: ex:MusicalWork ]"
+    assert str(list(a.subClassOf)) == "[Class: ex:MusicalWork ]"
     # [Class: ex:MusicalWork ]
 
     # This can also be used against already populated graphs:
@@ -52,76 +51,75 @@ def test_infix_owl_example1():
     namespace_manager.bind("owl", OWL, override=False)
     owlGraph.namespace_manager = namespace_manager
     assert (
-        repr(list(Class(OWL.Class, graph=owlGraph).subClassOf))
+        str(list(Class(OWL.Class, graph=owlGraph).subClassOf))
         == "[Class: rdfs:Class ]"
     )
 
     # Operators are also available. For instance we can add ex:Opera to the extension
     # of the ex:CreativeWork class via the '+=' operator
 
-    assert repr(a) == "Class: ex:Opera SubClassOf: ex:MusicalWork"
+    assert str(a) == "Class: ex:Opera SubClassOf: ex:MusicalWork"
     b = Class(exNs.CreativeWork, graph=g)
     b += a
     assert (
-        repr(sorted(a.subClassOf, key=lambda c: c.identifier))
+        str(sorted(a.subClassOf, key=lambda c: c.identifier))
         == "[Class: ex:CreativeWork , Class: ex:MusicalWork ]"
     )
 
     # And we can then remove it from the extension as well
 
     b -= a
-    assert repr(a) == "Class: ex:Opera SubClassOf: ex:MusicalWork"
+    assert str(a) == "Class: ex:Opera SubClassOf: ex:MusicalWork"
 
     # Boolean class constructions can also  be created with Python operators.
     # For example, The | operator can be used to construct a class consisting of a
     # owl:unionOf the operands:
 
     c = a | b | Class(exNs.Work, graph=g)
-    assert repr(c) == "( ex:Opera OR ex:CreativeWork OR ex:Work )"
+    assert str(c) == "( ex:Opera OR ex:CreativeWork OR ex:Work )"
 
     # Boolean class expressions can also be operated as lists (using python list
     # operators)
 
     del c[c.index(Class(exNs.Work, graph=g))]
-    assert repr(c) == "( ex:Opera OR ex:CreativeWork )"
+    assert str(c) == "( ex:Opera OR ex:CreativeWork )"
 
     # The '&' operator can be used to construct class intersection:
 
     woman = Class(exNs.Female, graph=g) & Class(exNs.Human, graph=g)
     woman.identifier = exNs.Woman
-    assert repr(woman) == "( ex:Female AND ex:Human )"
+    assert str(woman) == "( ex:Female AND ex:Human )"
     assert len(woman) == 2
 
     # Enumerated classes can also be manipulated
 
     contList = [Class(exNs.Africa, graph=g), Class(exNs.NorthAmerica, graph=g)]
     assert (
-        repr(EnumeratedClass(members=contList, graph=g))
+        str(EnumeratedClass(members=contList, graph=g))
         == "{ ex:Africa ex:NorthAmerica }"
     )
 
     # owl:Restrictions can also be instantiated:
 
     assert (
-        repr(Restriction(exNs.hasParent, graph=g, allValuesFrom=exNs.Human))
+        str(Restriction(exNs.hasParent, graph=g, allValuesFrom=exNs.Human))
         == "( ex:hasParent ONLY ex:Human )"
     )
 
     # Restrictions can also be created using Manchester OWL syntax in 'colloquial'
     # Python
     assert (
-        repr(exNs.hasParent << some >> Class(exNs.Physician, graph=g))
+        str(exNs.hasParent << some >> Class(exNs.Physician, graph=g))
         == "( ex:hasParent SOME ex:Physician )"
     )
 
     Property(exNs.hasParent, graph=g) << max >> Literal(1)
     assert (
-        repr(Property(exNs.hasParent, graph=g) << max >> Literal(1))
+        str(Property(exNs.hasParent, graph=g) << max >> Literal(1))
         == "( ex:hasParent MAX 1 )"
     )
 
 
-# @pytest.mark.skip
 def test_infixowl_deepclassclear():
     EX = Namespace("http://example.com/")
 
@@ -141,33 +139,33 @@ def test_infixowl_deepclassclear():
 
     anonClass = EX.someProp << some >> classD
 
-    assert repr(anonClass) == "( ex:someProp SOME ex:D )"
-    assert repr(classF) == "Class: ex:F "
+    assert str(anonClass) == "( ex:someProp SOME ex:D )"
+    assert str(classF) == "Class: ex:F "
 
     classF += anonClass
 
-    assert repr(list(anonClass.subClassOf)) == "[Class: ex:F ]"
+    assert str(list(anonClass.subClassOf)) == "[Class: ex:F ]"
 
     classA = classE | classF | anonClass
     classB += classA
     classA.equivalentClass = [Class()]
     classB.subClassOf = [EX.someProp << some >> classC]
 
-    assert repr(classA) == "( ex:E OR ex:F OR ( ex:someProp SOME ex:D ) )"
+    assert str(classA) == "( ex:E OR ex:F OR ( ex:someProp SOME ex:D ) )"
 
     DeepClassClear(classA)
-    assert repr(classA) == "(  )"
+    assert str(classA) == "(  )"
 
     assert list(anonClass.subClassOf) == []
 
-    assert repr(classB) == "Class: ex:B SubClassOf: ( ex:someProp SOME ex:C )"
+    assert str(classB) == "Class: ex:B SubClassOf: ( ex:someProp SOME ex:C )"
 
     otherClass = classD | anonClass
 
-    assert repr(otherClass) == "( ex:D OR ( ex:someProp SOME ex:D ) )"
+    assert str(otherClass) == "( ex:D OR ( ex:someProp SOME ex:D ) )"
 
     DeepClassClear(otherClass)
-    assert repr(otherClass) == "(  )"
+    assert str(otherClass) == "(  )"
 
     otherClass.delete()
     assert list(g.triples((otherClass.identifier, None, None))) == []
@@ -218,7 +216,7 @@ def test_infixowl_class_and():
     youngPerson = Class(exNs.YoungPerson, graph=g)
     youngWoman = female & human & youngPerson
 
-    assert repr(youngWoman) == "ex:YoungPerson THAT ( ex:Female AND ex:Human )"
+    assert str(youngWoman) == "ex:YoungPerson THAT ( ex:Female AND ex:Human )"
 
     assert isinstance(youngWoman, BooleanClass) is True
     assert isinstance(youngWoman.identifier, BNode) is True
@@ -241,10 +239,10 @@ def test_infix_owl_class_getparents():
     sibling.identifier = exNs.Sibling
     assert len(sibling) == 2
 
-    assert repr(sibling) == "( ex:Brother OR ex:Sister )"
+    assert str(sibling) == "( ex:Brother OR ex:Sister )"
 
     assert (
-        repr(first(brother.parents))
+        str(first(brother.parents))
         == "Class: ex:Sibling EquivalentTo: ( ex:Brother OR ex:Sister )"
     )
     parent = Class(exNs.Parent)
@@ -252,7 +250,7 @@ def test_infix_owl_class_getparents():
     father = parent & male
     father.identifier = exNs.Father
     assert len(list(father.parents)) == 2
-    assert repr(list(father.parents)) == "[Class: ex:Parent , Class: ex:Male ]"
+    assert str(list(father.parents)) == "[Class: ex:Parent , Class: ex:Male ]"
 
 
 def test_infixowl_enumeratedclass():
@@ -267,18 +265,18 @@ def test_infixowl_enumeratedclass():
     ogbujiBros = EnumeratedClass(
         exNs.ogbujicBros, members=[exNs.chime, exNs.uche, exNs.ejike]
     )
-    assert repr(ogbujiBros) == "{ ex:chime ex:uche ex:ejike }"
+    assert str(ogbujiBros) == "{ ex:chime ex:uche ex:ejike }"
 
     col = Collection(
         g, first(g.objects(predicate=OWL.oneOf, subject=ogbujiBros.identifier))
     )
     assert (
-        repr(sorted([g.qname(item) for item in col]))
+        str(sorted([g.qname(item) for item in col]))
         == "['ex:chime', 'ex:ejike', 'ex:uche']"
     )
     # logger.debug(g.serialize(format="pretty-xml"))
 
-    assert repr(g.serialize(format="n3")) == repr(
+    assert str(g.serialize(format="n3")) == str(
         "@prefix ex: <http://example.com/> .\n"
         "@prefix owl: <http://www.w3.org/2002/07/owl#> .\n"
         "@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .\n"
@@ -304,10 +302,10 @@ def test_infixowl_booleanclassextenthelper():
     water = Class(EX.Water)
 
     testClass = BooleanClass(members=[fire, water])
-    assert repr(testClass) == "( ex:Fire AND ex:Water )"
+    assert str(testClass) == "( ex:Fire AND ex:Water )"
 
     testClass2 = BooleanClass(operator=OWL.unionOf, members=[fire, water])
-    assert repr(testClass2) == "( ex:Fire OR ex:Water )"
+    assert str(testClass2) == "( ex:Fire OR ex:Water )"
 
 
 def test_infixowl_changeoperator():
@@ -324,7 +322,7 @@ def test_infixowl_changeoperator():
     water = Class(EX.Water)
     testClass = BooleanClass(members=[fire, water])
 
-    assert repr(testClass) == "( ex:Fire AND ex:Water )"
+    assert str(testClass) == "( ex:Fire AND ex:Water )"
 
     testClass.changeOperator(OWL.unionOf)
     assert repr(testClass) == "( ex:Fire OR ex:Water )"
@@ -332,7 +330,7 @@ def test_infixowl_changeoperator():
     try:
         testClass.changeOperator(OWL.unionOf)
     except Exception as e:
-        assert repr(e) == "AssertionError('The new operator is already being used!')"
+        assert str(e) == "The new operator is already being used!"
 
 
 def test_infixowl_serialization():
@@ -351,10 +349,10 @@ def test_infixowl_serialization():
 
     restr1 = prop << some >> (Class(EX.Foo))
 
-    assert repr(restr1) == "( ex:someProp SOME ex:Foo )"
+    assert str(restr1) == "( ex:someProp SOME ex:Foo )"
 
     assert (
-        repr(list(Property(EX.someProp, baseType=None).type))
+        str(list(Property(EX.someProp, baseType=None).type))
         == "[rdflib.term.URIRef('http://www.w3.org/2002/07/owl#DatatypeProperty')]"
     )
 
@@ -371,5 +369,51 @@ def test_cardinality_zero():
     )
 
 
-if __name__ == "__main__":
-    test_cardinality_zero()
+def test_lshift_rlshift_delimiters():
+    EX = Namespace("http://example.com/")
+    namespace_manager = NamespaceManager(Graph())
+    namespace_manager.bind("ex", EX, override=False)
+    namespace_manager.bind("owl", OWL, override=False)
+    g = Graph()
+    g.namespace_manager = namespace_manager
+    Individual.factoryGraph = g
+    classB = Class(EX.B)
+    classC = Class(EX.C)
+    classD = Class(EX.D)
+    classE = Class(EX.E)
+    classF = Class(EX.F)
+
+    anonClass = EX.someProp << some >> classD
+    classF += anonClass
+    assert str(list(anonClass.subClassOf)) == "[Class: ex:F ]"
+
+    classA = classE | classF | anonClass
+    classB += classA
+    classA.equivalentClass = [Class()]
+    classB.subClassOf = [EX.someProp << some >> classC]
+    assert str(classA) == "( ex:E OR ex:F OR ( ex:someProp SOME ex:D ) )"
+
+
+def test_matmul_rmatmul_delimiters():
+    EX = Namespace("http://example.com/")
+    namespace_manager = NamespaceManager(Graph())
+    namespace_manager.bind("ex", EX, override=False)
+    namespace_manager.bind("owl", OWL, override=False)
+    g = Graph()
+    g.namespace_manager = namespace_manager
+    Individual.factoryGraph = g
+    classB = Class(EX.B)
+    classC = Class(EX.C)
+    classD = Class(EX.D)
+    classE = Class(EX.E)
+    classF = Class(EX.F)
+
+    anonClass = EX.someProp @ some @ classD
+    classF += anonClass
+    assert str(list(anonClass.subClassOf)) == "[Class: ex:F ]"
+
+    classA = classE | classF | anonClass
+    classB += classA
+    classA.equivalentClass = [Class()]
+    classB.subClassOf = [EX.someProp @ some @ classC]
+    assert str(classA) == "( ex:E OR ex:F OR ( ex:someProp SOME ex:D ) )"
