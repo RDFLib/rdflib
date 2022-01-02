397fc296a30b2fac165a8c4e22f3ecd889d71c53
Nicholas Car <nicholas.car@surroundaustralia.com>
2022-01-02 18:34:33 +1000

    Update parser.py

--- rdflib/parser.py
+++ rdflib/parser.py
@@ -185,7 +185,7 @@ class URLInputSource(InputSource):
             myheaders["Accept"] = "application/rdf+xml, */*;q=0.1"
         elif format == "n3":
             myheaders["Accept"] = "text/n3, */*;q=0.1"
-        elif format in ["turtle" or "ttl"]:
+        elif format in ["turtle", "ttl"]:
             myheaders["Accept"] = "text/turtle, application/x-turtle, */*;q=0.1"
         elif format == "nt":
             myheaders["Accept"] = "text/plain, */*;q=0.1"
