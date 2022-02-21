435534c8ddc0290c6c95f039f92d556384de799e
Nicholas Car <nicholas.car@surroundaustralia.com>
2022-02-21 18:06:15 +1000

    changed Datatype to Class, removed geometry

--- rdflib/namespace/_WGS.py
+++ rdflib/namespace/_WGS.py
@@ -12,18 +12,12 @@ class WGS(DefinedNamespace):
 
     _NS = Namespace("https://www.w3.org/2003/01/geo/wgs84_pos#")
 
-    # http://www.w3.org/2000/01/rdf-schema#Datatype
+    # http://www.w3.org/2000/01/rdf-schema#Class
     SpatialThing: URIRef
     Point: URIRef
 
     # http://www.w3.org/2002/07/owl#DatatypeProperty
     alt: URIRef
-
-    # NOTE: this is not in the official vocabulary, but it is used by dbpedia
-    # and will commonly be treated as a de-facto term. It's range appears to be
-    # a structured string literal. E.g.,
-    # > _:something geo:geometry "POINT(-76.348335266113 39.536666870117)"
-    geometry: URIRef
 
     lat: URIRef  # http://www.w3.org/2003/01/geo/wgs84_pos#lat
     lat_long: URIRef
