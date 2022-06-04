e5ccbc5e1e66bf29af55d88bb8f0c5f89ac01105
Graham Higgins <gjhiggins@users.noreply.github.com>
2022-06-05 00:31:48 +0200

    rewrote the test as pytest-compatible

--- test/test_having.py
+++ test/test_having.py
@@ -1,43 +1,36 @@
-import sys
-import unittest
+import pytest
 from rdflib import Graph
 
-class GraphTestCase(unittest.TestCase):
-    g = Graph()
-    def setUp(self):
-        data = """
-        <urn:a> <urn:p> 1 .
-        <urn:b> <urn:p> 3 .
-        <urn:c> <urn:q> 1 .
-        """
+g = Graph()
 
-        self.g.parse(data=data, format="turtle")
-
-    def testGroupBy(self):
-        query = ("SELECT ?p ?o "
-                 "WHERE { ?s ?p ?o } "
-                 "GROUP BY ?p")
-        qres = self.g.query(query)
-
-        self.assertEqual(2, len(qres))
-
-    def testHavingAggregateEqLiteral(self):
-
-        query = ("SELECT ?p (avg(?o) as ?a) "
-                 "WHERE { ?s ?p ?o } "
-                 "GROUP BY ?p HAVING (avg(?o) = 2 )")
-        qres = self.g.query(query)
-
-        self.assertEqual(1, len(qres))
-
-    def testHavingPrimaryExpressionVarNeqIri(self):
-        query = ("SELECT ?p ?o "
-                 "WHERE { ?s ?p ?o } "
-                 "GROUP BY ?p HAVING (?p != <urn:foo> )")
-        qres = self.g.query(query)
-
-        self.assertEqual(2, len(qres))
+data = """
+<urn:a> <urn:p> 1 .
+<urn:b> <urn:p> 3 .
+<urn:c> <urn:q> 1 .
+"""
+g.parse(data=data, format="turtle")
 
 
-if __name__ == '__main__':
-    unittest.main(argv=sys.argv[:1])
+def test_group_by():
+    query = "SELECT ?p" "WHERE { ?s ?p ?o } " "GROUP BY ?p"
+    qres = g.query(query)
+
+    assert len(qres) == 2
+
+
+def test_having_aggregate_eq_literal():
+    query = (
+        "SELECT ?p (avg(?o) as ?a) "
+        "WHERE { ?s ?p ?o } "
+        "GROUP BY ?p HAVING (avg(?o) = 2 )"
+    )
+    qres = g.query(query)
+
+    assert len(qres) == 1
+
+
+def test_having_primary_expression_var_neq_iri():
+    query = "SELECT ?p " "WHERE { ?s ?p ?o } " "GROUP BY ?p HAVING (?p != <urn:foo> )"
+    qres = g.query(query)
+
+    assert len(qres) == 2
