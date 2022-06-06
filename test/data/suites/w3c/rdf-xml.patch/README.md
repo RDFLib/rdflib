40568c720b343f1ae44a87bd22e7c0e9f4e1209a
Iwan Aucamp <aucampia@gmail.com>
2022-06-06 12:19:43 +0200

    Move non-normative W3C RDF/XML tests into a separate suite
    This is so we don't report on non-normative tests.

--- test/data/suites/w3c/rdf-xml.patch/README.md
+++
@@ -1,9 +0,0 @@
-# `rdf-xml.patch` notes
-
-Some tests are commented out in
-<https://www.w3.org/2013/RDFXMLTests/manifest.ttl>. These test pass with our
-test runner and cover functionality in the RDF/XML parser that is not otherwise
-covered.
-
-This patch directory contains a manifest that has the commented-out tests
-restored, but is otherwise identical.
--- test/test_w3c_spec/test_rdfxml_w3c.py
+++ test/test_w3c_spec/test_rdfxml_w3c.py
@@ -62,3 +62,23 @@ def check_entry(entry: ManifestEntry) -> None:
 )
 def test_entry(manifest_entry: ManifestEntry) -> None:
     check_entry(manifest_entry)
+
+
+REMOTE_BASE_IRI = "http://www.w3.org/2013/RDFXMLTests/"
+NON_NORMATIVE_LOCAL_BASE_DIR = TEST_DATA_DIR / "suites/w3c/rdf-xml-non-normative/"
+NON_NORMATIVE_MAPPER = URIMapper.from_mappings(
+    (REMOTE_BASE_IRI, ensure_suffix(NON_NORMATIVE_LOCAL_BASE_DIR.as_uri(), "/"))
+)
+
+
+@pytest.mark.parametrize(
+    ["manifest_entry"],
+    params_from_sources(
+        NON_NORMATIVE_MAPPER,
+        ManifestEntry,
+        NON_NORMATIVE_LOCAL_BASE_DIR / "manifest.ttl",
+        report_prefix="rdflib_w3c_rdfxml_non_normative",
+    ),
+)
+def test_non_normative_entry(manifest_entry: ManifestEntry) -> None:
+    check_entry(manifest_entry)
