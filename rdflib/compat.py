831303b875c423788799b9e6268c4714fa0202c4
Graham Higgins <gjh@bel-epa.com>
2022-07-21 17:55:20 +0100

    oh, we _are_ doing F401s

--- rdflib/compat.py
+++ rdflib/compat.py
@@ -14,7 +14,7 @@ else:
     try:
         from lxml import etree  # n0qa: N813
     except ImportError:
-        import xml.etree.ElementTree as etree  # noqa: N813
+        import xml.etree.ElementTree as etree  # noqa: N813 F401
 
 
 def cast_bytes(s, enc="utf-8"):
