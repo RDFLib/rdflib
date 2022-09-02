ff9c99e827e1201db25186cb22310f60c3fe7b78
Miel Vander Sande <miel.vandersande@meemoo.be>
2022-09-02 15:05:30 +0200

    Add test for polist issue

--- test/test_sparql/test_sparql.py
+++ test/test_sparql/test_sparql.py
@@ -89,6 +89,30 @@ def test_sparql_bnodelist():
     prepareQuery("select * where { ?s ?p ( [ ?p2 ?o2 ] [] ) . }")
     prepareQuery("select * where { ?s ?p ( [] [ ?p2 ?o2 ] [] ) . }")
 
+@pytest.mark.xfail
+def test_sparql_polist():
+    """
+
+    syntax tests for equivalence object and predicate-object lists
+
+    """
+
+    g = Graph()
+    g.parse(
+        data="""
+    @prefix : <urn:ns1:> .
+    :s :p [ :v 1 ], [ :v 2].
+    """,
+        format="n3",
+    )
+
+    qres1 = g.query("PREFIX : <urn:ns1:> select * where { ?s :p [ ], [ ] . }")
+    qres2 = g.query("PREFIX : <urn:ns1:> select * where { ?s :p [ ]; :p [ ] . }")
+    assert len(qres1) == len(qres2)
+
+    qres3 = g.query("PREFIX : <urn:ns1:> select ?v1 ?v2 where { ?s :p [ :v ?v1 ], [ :v ?v2] . }")
+    qres4 = g.query("PREFIX : <urn:ns1:> select ?v1 ?v2 where { ?s :p [ :v ?v1 ]; :p [ :v ?v2 ] . }")
+    assert len(qres3) == len(qres4)
 
 def test_complex_sparql_construct():
 
