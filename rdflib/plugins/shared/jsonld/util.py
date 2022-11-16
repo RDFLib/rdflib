53304a1b8af977f2eb5df8d83d2e7958d449ab29
zero0nee <kristian.ronn.89@gmail.com>
2022-11-16 13:03:13 +0100

    Issue #2098

--- rdflib/plugins/shared/jsonld/util.py
+++ rdflib/plugins/shared/jsonld/util.py
@@ -13,7 +13,8 @@ else:
         import simplejson as json
 
 from io import TextIOBase, TextIOWrapper
-from posixpath import normpath, sep
+from posixpath import normpath
+from os import sep
 from urllib.parse import urljoin, urlsplit, urlunsplit
 
 from rdflib.parser import (
