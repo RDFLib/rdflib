f87272047ecbebf7f9b76ab9dd118afd20da4502
Charles Tapley Hoyt <cthoyt@gmail.com>
2023-02-27 19:24:00 +0100

    Update bind_prefix_cc.py

--- examples/bind_prefix_cc.py
+++ examples/bind_prefix_cc.py
@@ -7,4 +7,12 @@ this resource.
 import rdflib
 
 graph = rdflib.Graph(bind_namespaces="cc")
+
+# The Gene Ontology is a biomedical ontology describing
+# biological processes, cellular locations, and cellular components.
+# It is typically abbreviated with the prefix "go" and uses PURLs
+# issued by the Open Biological and Biomedical Ontologies Foundry.
+prefix_map = {prefix: str(ns) for prefix, ns in graph.namespaces()}
+assert "go" in prefix_map
+assert prefix_map["go"] == "http://purl.obolibrary.org/obo/GO_"
 assert graph.qname("http://purl.obolibrary.org/obo/GO_0032571") == "go:0032571"
