35e5543d49b5847e218e1629fdd4174f068a3244
Iwan Aucamp <aucampia@gmail.com>
2023-08-25 17:47:41 +0200

    test: make graph variant tests more granular (#2540)
    The graph variant tests were checking all variants in one test, so if
    one format had issues, the whole variant was failing. The result was
    overly broad xfails.
    
    This change splits the variant tests up into separate tests for each
    variant.
    
    This change is made in preparation for adding Python based Graph variants.

--- test/utils/graph.py
+++ test/utils/graph.py
@@ -1,13 +1,18 @@
+from __future__ import annotations
+
 import logging
 from dataclasses import dataclass
 from functools import lru_cache
 from pathlib import Path
-from typing import Optional, Tuple, Union
+from typing import Optional, Tuple, Type, Union
 
-from rdflib.graph import Graph
+import rdflib.util
+from rdflib.graph import Graph, _GraphT
 from rdflib.util import guess_format
 
 GraphSourceType = Union["GraphSource", Path]
+
+SUFFIX_FORMAT_MAP = {**rdflib.util.SUFFIX_FORMAT_MAP, "hext": "hext"}
 
 
 @dataclass(frozen=True)
@@ -17,11 +22,22 @@ class GraphSource:
     public_id: Optional[str] = None
 
     @classmethod
-    def from_path(cls, path: Path, public_id: Optional[str] = None) -> "GraphSource":
-        format = guess_format(f"{path}")
+    def guess_format(cls, path: Path) -> Optional[str]:
+        format: Optional[str]
+        if path.suffix == ".py":
+            format = "python"
+        else:
+            format = guess_format(f"{path}", SUFFIX_FORMAT_MAP)
+        return format
+
+    @classmethod
+    def from_path(
+        cls, path: Path, public_id: Optional[str] = None, format: Optional[str] = None
+    ) -> GraphSource:
+        if format is None:
+            format = cls.guess_format(path)
         if format is None:
             raise ValueError(f"could not guess format for source {path}")
-
         return cls(path, format, public_id)
 
     @classmethod
@@ -41,10 +57,15 @@ class GraphSource:
         return source
 
     def load(
-        self, graph: Optional[Graph] = None, public_id: Optional[str] = None
-    ) -> Graph:
+        self,
+        graph: Optional[_GraphT] = None,
+        public_id: Optional[str] = None,
+        # type error: Incompatible default for argument "graph_type" (default has type "Type[Graph]", argument has type "Type[_GraphT]")
+        # see https://github.com/python/mypy/issues/3737
+        graph_type: Type[_GraphT] = Graph,  # type: ignore[assignment]
+    ) -> _GraphT:
         if graph is None:
-            graph = Graph()
+            graph = graph_type()
         graph.parse(
             source=self.path,
             format=self.format,
@@ -55,11 +76,12 @@ class GraphSource:
 
 def load_sources(
     *sources: GraphSourceType,
-    graph: Optional[Graph] = None,
+    graph: Optional[_GraphT] = None,
     public_id: Optional[str] = None,
-) -> Graph:
+    graph_type: Type[_GraphT] = Graph,  # type: ignore[assignment]
+) -> _GraphT:
     if graph is None:
-        graph = Graph()
+        graph = graph_type()
     for source in sources:
         GraphSource.from_source(source).load(graph, public_id)
     return graph
@@ -67,6 +89,8 @@ def load_sources(
 
 @lru_cache(maxsize=None)
 def cached_graph(
-    sources: Tuple[Union[GraphSource, Path], ...], public_id: Optional[str] = None
-) -> Graph:
-    return load_sources(*sources, public_id=public_id)
+    sources: Tuple[Union[GraphSource, Path], ...],
+    public_id: Optional[str] = None,
+    graph_type: Type[_GraphT] = Graph,  # type: ignore[assignment]
+) -> _GraphT:
+    return load_sources(*sources, public_id=public_id, graph_type=graph_type)
