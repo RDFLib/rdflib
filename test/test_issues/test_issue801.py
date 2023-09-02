39f6472d509657a4e1f27269eb7a8d6ec5dc34a7
Iwan Aucamp <aucampia@gmail.com>
2023-09-02 23:17:11 +0200

    test: convert more unittest based tests to pytest (#2564)
    All tests should be using pytests.

--- test/test_issues/test_issue801.py
+++ test/test_issues/test_issue801.py
@@ -1,22 +1,14 @@
 """
 Issue 801 - Problem with prefixes created for URIs containing %20
 """
-import unittest
 from test.utils.namespace import EGDO
 
 from rdflib import BNode, Graph, Literal
 
 
-class TestIssue801(unittest.TestCase):
-    def test_issue_801(self):
-        g = Graph()
-        g.bind("", EGDO)
-        node = BNode()
-        g.add((node, EGDO["first%20name"], Literal("John")))
-        self.assertEqual(
-            g.serialize(format="turtle").split("\n")[-3], '[] :first%20name "John" .'
-        )
-
-
-if __name__ == "__main__":
-    unittest.main()
+def test_issue_801():
+    g = Graph()
+    g.bind("", EGDO)
+    node = BNode()
+    g.add((node, EGDO["first%20name"], Literal("John")))
+    assert g.serialize(format="turtle").split("\n")[-3] == '[] :first%20name "John" .'
