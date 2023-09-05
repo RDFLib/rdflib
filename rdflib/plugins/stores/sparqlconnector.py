10df590b6e97079ec757455699685b68883fff9c
Iwan Aucamp <aucampia@gmail.com>
2023-09-05 23:37:46 +0000

    aucampia/20230905T2007-add-future-annotations: checkpoint 20230906T013746

--- rdflib/plugins/stores/sparqlconnector.py
+++ rdflib/plugins/stores/sparqlconnector.py
@@ -42,7 +42,7 @@ class SPARQLConnector:
         query_endpoint: Optional[str] = None,
         update_endpoint: Optional[str] = None,
         returnFormat: str = "xml",  # noqa: N803
-        method: te.Literal['GET', 'POST', 'POST_FORM'] = "GET",
+        method: te.Literal["GET", "POST", "POST_FORM"] = "GET",
         auth: Optional[Tuple[str, str]] = None,
         **kwargs,
     ):
