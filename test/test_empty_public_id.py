fbb4f2a2db6d64b881a9323f9e3dca0059a26b3f
Anton Lodder <alodder@opusonesolutions.com>
2023-11-25 23:06:54 -0500

    Add tests of empty publicID for all parsers

--- test/test_empty_public_id.py
+++ test/test_empty_public_id.py
@@ -1,3 +1,8 @@
+import pytest
+
+import rdflib
+from rdflib.term import URIRef
+
 """The tests in this file cover the use of an empty publicID
 (e.g. g.parse(sample, publicID=""). When `None` is passes to this argument,
 rdflib puts the file name for `sample` as a prefix to all rdf identifiers
@@ -28,37 +33,167 @@ resources.
 """
 
 
-def test_hext():
+def test_hext(tmp_path):
+    hext_sample = """\
+["#_AB16765A-B19E-4454-A58F-868D23C6CD26", "http://www.w3.org/1999/02/22-rdf-syntax-ns#type", "http://iec.ch/TC57/2013/CIM-schema-cim16#SwitchInfo", "globalId", "", ""]
+"""
+    sample_file = tmp_path / "sample.ndjson"
+    open(sample_file, "w").write(hext_sample)
+
+    g = rdflib.Graph()
+    g.parse(sample_file, format="hext", publicID="")
+
+    subject, predicate, obj = next(iter(g))
+    assert subject == URIRef("#_AB16765A-B19E-4454-A58F-868D23C6CD26")
+    assert predicate == URIRef("http://www.w3.org/1999/02/22-rdf-syntax-ns#type")
+    assert obj == URIRef("http://iec.ch/TC57/2013/CIM-schema-cim16#SwitchInfo")
+
+
+def test_jsonld(tmp_path):
+    jsonld_sample = """\
+{
+  "@context": {
+    "cim": "http://iec.ch/TC57/2013/CIM-schema-cim16#",
+    "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+  },
+  "@graph": [
+    {
+      "@id": "#_AB16765A-B19E-4454-A58F-868D23C6CD26",
+      "@type": "cim:SwitchInfo"
+    }
+  ]
+}"""
+
+    sample_file = tmp_path / "sample.json-ld"
+    open(sample_file, "w").write(jsonld_sample)
+
+    g = rdflib.Graph()
+    g.parse(sample_file, format="json-ld", publicID="")
+
+    subject, predicate, obj = next(iter(g))
+
+    assert subject == URIRef("#_AB16765A-B19E-4454-A58F-868D23C6CD26")
+    assert predicate == URIRef("http://www.w3.org/1999/02/22-rdf-syntax-ns#type")
+    assert obj == URIRef("http://iec.ch/TC57/2013/CIM-schema-cim16#SwitchInfo")
+
+
+@pytest.mark.parametrize("fmt", ["n3", "turtle"])
+def test_notation3(tmp_path, fmt):
+    """Test notation3 (i.e. .ttl) files can be parsed with publicID of"""
+    ttl_sample = """\
+    <#_AB16765A-B19E-4454-A58F-868D23C6CD26> a
+    <http://iec.ch/TC57/2013/CIM-schema-cim16#SwitchInfo> .
+    """
+
+    sample_file = tmp_path / "sample.ttl"
+    open(sample_file, "w").write(ttl_sample)
+
+    g = rdflib.Graph()
+    g.parse(sample_file, format=fmt, publicID="")
+
+    subject, predicate, obj = next(iter(g))
+
+    assert subject == URIRef("#_AB16765A-B19E-4454-A58F-868D23C6CD26")
+    assert predicate == URIRef("http://www.w3.org/1999/02/22-rdf-syntax-ns#type")
+    assert obj == URIRef("http://iec.ch/TC57/2013/CIM-schema-cim16#SwitchInfo")
+
+
+def test_nquads(tmp_path):
+    nquads_sample = """\
+<#_AB16765A-B19E-4454-A58F-868D23C6CD26> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://iec.ch/TC57/2013/CIM-schema-cim16#SwitchInfo> .
+"""
+    sample_file = tmp_path / "sample.nquads"
+    open(sample_file, "w").write(nquads_sample)
+
+    g = rdflib.Graph()
+    g.parse(sample_file, format="nquads", publicID="http://foo.com")
+
+    subject, predicate, obj = next(iter(g))
+    assert subject == URIRef("#_AB16765A-B19E-4454-A58F-868D23C6CD26")
+    assert predicate == URIRef("http://www.w3.org/1999/02/22-rdf-syntax-ns#type")
+    assert obj == URIRef("http://iec.ch/TC57/2013/CIM-schema-cim16#SwitchInfo")
+
+
+def test_ntriples(tmp_path):
+    ntriples_sample = """\
+<#_AB16765A-B19E-4454-A58F-868D23C6CD26> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://iec.ch/TC57/2013/CIM-schema-cim16#SwitchInfo> .
+"""
+    sample_file = tmp_path / "sample.ntriples"
+    open(sample_file, "w").write(ntriples_sample)
+
+    g = rdflib.Graph()
+    g.parse(sample_file, format="nt", publicID="")
+
+    subject, predicate, obj = next(iter(g))
+    assert subject == URIRef("#_AB16765A-B19E-4454-A58F-868D23C6CD26")
+    assert predicate == URIRef("http://www.w3.org/1999/02/22-rdf-syntax-ns#type")
+    assert obj == URIRef("http://iec.ch/TC57/2013/CIM-schema-cim16#SwitchInfo")
+
+
+def test_rdf_voc(tmp_path):
     ...
 
 
-def test_jsonld():
-    ...
+def test_rdfxml(tmp_path):
+    xml_sample = """\
+<?xml version="1.0" encoding="UTF-8"?>
+<rdf:RDF xmlns:cim="http://iec.ch/TC57/2013/CIM-schema-cim16#"
+         xmlns:cyme="http://www.cyme.com/CIM/1.0.2#"
+         xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" >
+    <cim:SwitchInfo rdf:ID="_AB16765A-B19E-4454-A58F-868D23C6CD26" />
+</rdf:RDF>"""
+
+    sample_file = tmp_path / "sample.xml"
+    open(sample_file, "w").write(xml_sample)
+
+    g = rdflib.Graph()
+    g.parse(sample_file, format="xml", publicID="")
+
+    subject, predicate, obj = next(iter(g))
+
+    assert subject == URIRef("#_AB16765A-B19E-4454-A58F-868D23C6CD26")
+    assert predicate == URIRef("http://www.w3.org/1999/02/22-rdf-syntax-ns#type")
+    assert obj == URIRef("http://iec.ch/TC57/2013/CIM-schema-cim16#SwitchInfo")
 
 
-def test_notation3():
-    ...
+def test_trig(tmp_path):
+    trig_sample = """\
+<#_AB16765A-B19E-4454-A58F-868D23C6CD26> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://iec.ch/TC57/2013/CIM-schema-cim16#SwitchInfo> .
+"""
+    sample_file = tmp_path / "sample.trig"
+    open(sample_file, "w").write(trig_sample)
+
+    g = rdflib.Graph()
+    g.parse(sample_file, format="trig", publicID="")
+
+    subject, predicate, obj = next(iter(g))
+    assert subject == URIRef("#_AB16765A-B19E-4454-A58F-868D23C6CD26")
+    assert predicate == URIRef("http://www.w3.org/1999/02/22-rdf-syntax-ns#type")
+    assert obj == URIRef("http://iec.ch/TC57/2013/CIM-schema-cim16#SwitchInfo")
 
 
-def test_nquads():
-    ...
+def test_trix(tmp_path):
+    trix_sample = """\
+<?xml version='1.0'?>
+<TriX xmlns='http://www.w3.org/2004/03/trix/trix-1/'>
+    <graph>
+        <uri>file:/home/grimnes/tmp/aperture/</uri>
+        <triple>
+            <uri>#_AB16765A-B19E-4454-A58F-868D23C6CD26</uri>
+            <uri>http://www.w3.org/1999/02/22-rdf-syntax-ns#type</uri>
+            <uri>http://iec.ch/TC57/2013/CIM-schema-cim16#SwitchInfo</uri>
+        </triple>
+  </graph>
+</TriX>"""
 
+    sample_file = tmp_path / "sample.trix"
+    open(sample_file, "w").write(trix_sample)
 
-def test_ntriples():
-    ...
+    g = rdflib.graph.ConjunctiveGraph()
+    g.parse(sample_file, format="trix", publicID="")
 
+    subject, predicate, obj = next(iter(g))
 
-def test_rdf_voc():
-    ...
-
-
-def test_rdfxml():
-    ...
-
-
-def test_trig():
-    ...
-
-
-def test_trix():
-    ...
+    assert subject == URIRef("#_AB16765A-B19E-4454-A58F-868D23C6CD26")
+    assert predicate == URIRef("http://www.w3.org/1999/02/22-rdf-syntax-ns#type")
+    assert obj == URIRef("http://iec.ch/TC57/2013/CIM-schema-cim16#SwitchInfo")
