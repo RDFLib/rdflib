afbeeaf674e30f2f8148df1b3fcd0499f3493e25
Anastasis Georgoulas <a.georgoulas@ucl.ac.uk>
2024-09-13 00:21:12 +0100

    Replace unittest with pytest in another test file
    Also simplifying by removing support for Python <2.7.

--- test/test_issues/test_issue379.py
+++ test/test_issues/test_issue379.py
@@ -1,58 +1,51 @@
-import unittest
+"""
+Tests for GitHub Issue 379: https://github.com/RDFLib/rdflib/issues/379
+"""
+
+import pytest
 
 import rdflib
 
-prefix_data = """
-              @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
-              @prefix : <http://www.example.com#> .
 
-              <http://www.example.com#prefix> a rdf:class ."""
+@pytest.fixture
+def prefix_data():
+    return """
+           @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+           @prefix : <http://www.example.com#> .
 
-base_data = """
-            @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
-            @base <http://www.example.com#> .
-
-            <http://www.example.com#base> a rdf:class .
-            """
+           <http://www.example.com#prefix> a rdf:class ."""
 
 
-class TestCase(unittest.TestCase):
-    def assertIsInstance(self, obj, cls, msg=None, *args, **kwargs):  # noqa: N802
-        """Python < v2.7 compatibility.  Assert 'obj' is instance of 'cls'"""
-        try:
-            f = super(TestCase, self).assertIsInstance
-        except AttributeError:
-            self.assertTrue(isinstance(obj, cls), *args, **kwargs)
-        else:
-            f(obj, cls, *args, **kwargs)
+@pytest.fixture
+def base_data():
+    return """
+           @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+           @base <http://www.example.com#> .
+
+           <http://www.example.com#base> a rdf:class .
+           """
 
 
-class TestBaseAllowsHash(TestCase):
+@pytest.fixture
+def graph():
+    return rdflib.Graph()
+
+
+def test_parse_successful_prefix_with_hash(graph, prefix_data):
     """
-    GitHub Issue 379: https://github.com/RDFLib/rdflib/issues/379
+    Test parse of '@prefix' namespace directive to allow a trailing hash '#', as is
+    permitted for an IRIREF:
+    http://www.w3.org/TR/2014/REC-turtle-20140225/#grammar-production-prefixID
     """
-
-    def setUp(self):
-        self.g = rdflib.Graph()
-
-    def test_parse_successful_prefix_with_hash(self):
-        """
-        Test parse of '@prefix' namespace directive to allow a trailing hash '#', as is
-        permitted for an IRIREF:
-        http://www.w3.org/TR/2014/REC-turtle-20140225/#grammar-production-prefixID
-        """
-        self.g.parse(data=prefix_data, format="n3")
-        self.assertIsInstance(next(self.g.subjects()), rdflib.URIRef)
-
-    def test_parse_successful_base_with_hash(self):
-        """
-        Test parse of '@base' namespace directive to allow a trailing hash '#', as is
-        permitted for an '@prefix' since both allow an IRIREF:
-        http://www.w3.org/TR/2014/REC-turtle-20140225/#grammar-production-base
-        """
-        self.g.parse(data=base_data, format="n3")
-        self.assertIsInstance(next(self.g.subjects()), rdflib.URIRef)
+    graph.parse(data=prefix_data, format="n3")
+    assert isinstance(next(graph.subjects()), rdflib.URIRef)
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_parse_successful_base_with_hash(graph, base_data):
+    """
+    Test parse of '@base' namespace directive to allow a trailing hash '#', as is
+    permitted for an '@prefix' since both allow an IRIREF:
+    http://www.w3.org/TR/2014/REC-turtle-20140225/#grammar-production-base
+    """
+    graph.parse(data=base_data, format="n3")
+    assert isinstance(next(graph.subjects()), rdflib.URIRef)
