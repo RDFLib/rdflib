6d744ceafa13e243773d9bac2e60ce455b81e37f
Edmond Chuc <edmond@kurrawong.ai>
2025-10-31 14:02:47 +1000

    chore: remove unused rdf4j testcontainer

--- test/test_rdf4j/conftest.py
+++ test/test_rdf4j/conftest.py
@@ -7,19 +7,7 @@ from testcontainers.core.waiting_utils import wait_for_logs
 
 from rdflib.contrib.rdf4j import RDF4JClient
 
-RDF4J_IMAGE = "eclipse/rdf4j-workbench:5.1.6-jetty"
-RDF4J_PORT = 8080
 GRAPHDB_PORT = 7200
-
-
-@pytest.fixture(scope="function")
-def rdf4j_container():
-    container = DockerContainer(RDF4J_IMAGE)
-    container.with_exposed_ports(RDF4J_PORT)
-    container.start()
-    wait_for_logs(container, "oejs.Server:main: Started")
-    yield container
-    container.stop()
 
 
 @pytest.fixture(scope="function")
