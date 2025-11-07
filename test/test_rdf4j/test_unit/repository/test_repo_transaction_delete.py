c67f7c27a32baf4388c280061e25365f74be2e0d
Edmond Chuc <edmond@kurrawong.ai>
2025-11-07 10:59:35 +1000

    test: update transaction tests for upload and delete

--- test/test_rdf4j/test_unit/repository/test_repo_transaction_delete.py
+++ test/test_rdf4j/test_unit/repository/test_repo_transaction_delete.py
@@ -10,14 +10,34 @@ from rdflib.contrib.rdf4j.client import (
 )
 
 
-def test_repo_transaction_delete(txn: Transaction, monkeypatch: pytest.MonkeyPatch):
+@pytest.mark.parametrize(
+    "base_uri, content_type, expected_headers, expected_params",
+    [
+        [None, None, {"Content-Type": "application/n-quads"}, {"action": "DELETE"}],
+        [
+            "http://example.com/",
+            "text/turtle",
+            {"Content-Type": "text/turtle"},
+            {"action": "DELETE", "baseURI": "http://example.com/"},
+        ],
+    ],
+)
+def test_repo_transaction_delete(
+    txn: Transaction,
+    monkeypatch: pytest.MonkeyPatch,
+    base_uri: str | None,
+    content_type: str | None,
+    expected_headers: dict[str, str],
+    expected_params: dict[str, str],
+):
     mock_response = Mock(spec=httpx.Response)
     mock_httpx_put = Mock(return_value=mock_response)
     monkeypatch.setattr(httpx.Client, "put", mock_httpx_put)
-    txn.delete("")
+    txn.delete("", base_uri, content_type)
+
     mock_httpx_put.assert_called_once_with(
         txn.url,
-        headers={"Content-Type": "application/n-quads"},
-        params={"action": "DELETE"},
+        headers=expected_headers,
+        params=expected_params,
         content=ANY,
     )
