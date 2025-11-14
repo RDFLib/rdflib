a215a82a47aba8b84279829755caff796997e9c6
Nicholas Car <nick@kurrawong.ai>
2025-11-14 18:14:28 +1000

    ensure description on 1 line

--- rdflib/tools/sparqlquery.py
+++ rdflib/tools/sparqlquery.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 """
-
-A commandline tool for querying with sparql
-on local files and remote sparql endpoints with custom serialization.
+A commandline tool for querying with SPARQL on local files and remote sparql endpoints with custom serialization.
 
 example usage:
 ```bash
