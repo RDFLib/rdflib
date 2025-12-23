6ccc01ad945810d814f94673ef73587daeb89560
Edmond Chuc <edmond@kurrawong.ai>
2025-12-23 16:35:20 +1000

    test: add e2e test for fgac rules add method

--- test/test_graphdb/test_e2e/repository_management/test_e2e_graphdb_fgac_rules.py
+++ test/test_graphdb/test_e2e/repository_management/test_e2e_graphdb_fgac_rules.py
@@ -45,3 +45,62 @@ def test_graphdb_fgac_rules_set(client: GraphDBClient):
     # Overwrite with an empty list of rules.
     repo.acl_rules.set([])
     assert repo.acl_rules.list() == []
+
+
+@pytest.mark.testcontainer
+def test_graphdb_fgac_rules_add(client: GraphDBClient):
+    repo = client.repositories.get("test-repo")
+    assert repo.acl_rules.list() == []
+    acl_rules_raw = [
+        {
+            "policy": "allow",
+            "role": "*",
+            "scope": "statement",
+            "operation": "*",
+            "subject": "*",
+            "predicate": "*",
+            "object": "*",
+            "context": "default",
+        },
+        {
+            "policy": "allow",
+            "role": "*",
+            "scope": "statement",
+            "operation": "*",
+            "subject": "*",
+            "predicate": "*",
+            "object": "*",
+            "context": "<urn:graph:test>",
+        },
+    ]
+    acl_rules = [AccessControlEntry.from_dict(x) for x in acl_rules_raw]
+    repo.acl_rules.add(acl_rules)
+    assert repo.acl_rules.list() == acl_rules
+
+    acl_rule_2 = AccessControlEntry.from_dict({
+        "policy": "allow",
+        "role": "*",
+        "scope": "statement",
+        "operation": "*",
+        "subject": "*",
+        "predicate": "*",
+        "object": "*",
+        "context": "<urn:graph:test2>",
+    })
+    repo.acl_rules.add([acl_rule_2])
+    # Rule is added to the end.
+    assert repo.acl_rules.list() == acl_rules + [acl_rule_2]
+
+    acl_rule_3 = AccessControlEntry.from_dict({
+        "policy": "allow",
+        "role": "*",
+        "scope": "statement",
+        "operation": "*",
+        "subject": "*",
+        "predicate": "*",
+        "object": "*",
+        "context": "<urn:graph:test3>",
+    })
+    repo.acl_rules.add([acl_rule_3], position=1)
+    # Rule is added to the specified position.
+    assert repo.acl_rules.list() == [acl_rules[0], acl_rule_3, acl_rules[1], acl_rule_2]
