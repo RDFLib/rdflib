c74f4f0cae393546e4e06ada963a11dd91a50eca
gjhiggins <gjhiggins@gmail.com>
2026-05-12 11:08:25 +0100

    blacked

--- test/test_misc/test_void.py
+++ test/test_misc/test_void.py
@@ -4,31 +4,31 @@ from rdflib import Graph, URIRef
 
 
 data = (
-    '@prefix void: <http://rdfs.org/ns/void#> .\n'
-    '@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .\n'
-    '@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .\n'
-    '@prefix owl: <http://www.w3.org/2002/07/owl#> .\n'
-    '@prefix xsd: <http://www.w3.org/2001/XMLSchema#> .\n'
-    '@prefix dcterms: <http://purl.org/dc/terms/> .\n'
-    '@prefix foaf: <http://xmlns.com/foaf/0.1/> .\n'
-    '@prefix wv: <http://vocab.org/waiver/terms/norms> .        \n'
-    '@prefix sd: <http://www.w3.org/ns/sparql-service-description#> .\n'
-    '@prefix : <#> .\n'
-    '\n'
-    ':DBpedia a void:Dataset;\n'
+    "@prefix void: <http://rdfs.org/ns/void#> .\n"
+    "@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .\n"
+    "@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .\n"
+    "@prefix owl: <http://www.w3.org/2002/07/owl#> .\n"
+    "@prefix xsd: <http://www.w3.org/2001/XMLSchema#> .\n"
+    "@prefix dcterms: <http://purl.org/dc/terms/> .\n"
+    "@prefix foaf: <http://xmlns.com/foaf/0.1/> .\n"
+    "@prefix wv: <http://vocab.org/waiver/terms/norms> .        \n"
+    "@prefix sd: <http://www.w3.org/ns/sparql-service-description#> .\n"
+    "@prefix : <#> .\n"
+    "\n"
+    ":DBpedia a void:Dataset;\n"
     '    dcterms:title "DBPedia";\n'
     '    dcterms:description "RDF data extracted from Wikipedia";\n'
-    '    dcterms:contributor :FU_Berlin;\n'
-    '    dcterms:contributor :University_Leipzig;\n'
-    '    dcterms:contributor :OpenLink_Software;\n'
-    '    dcterms:contributor :DBpedia_community;\n'
-    '    dcterms:source <http://dbpedia.org/resource/Wikipedia>;\n'
+    "    dcterms:contributor :FU_Berlin;\n"
+    "    dcterms:contributor :University_Leipzig;\n"
+    "    dcterms:contributor :OpenLink_Software;\n"
+    "    dcterms:contributor :DBpedia_community;\n"
+    "    dcterms:source <http://dbpedia.org/resource/Wikipedia>;\n"
     '    dcterms:modified "2008-11-17"^^xsd:date;\n'
-    '    .\n'
-    ':FU_Berlin a foaf:Organization;\n'
+    "    .\n"
+    ":FU_Berlin a foaf:Organization;\n"
     '    rdfs:label "Freie Universität Berlin";\n'
-    '    foaf:homepage <http://www.fu-berlin.de/>;\n'
-    '    .\n'
+    "    foaf:homepage <http://www.fu-berlin.de/>;\n"
+    "    .\n"
 )
 
 dataset = URIRef("http://dbpedia.org")
