6b91089f54e1d7a070866d05397c623f81f2d71a
w3lld1 <42353747+w3lld1@users.noreply.github.com>
2026-07-12 21:13:15 +0200

    style: format Turtle parser regression tests

--- test/test_n3.py
+++ test/test_n3.py
@@ -12,9 +12,12 @@ from test import TEST_DIR
 
 def test_bad_syntax_exposes_error_message() -> None:
     with pytest.raises(BadSyntax) as raised:
-        Graph().parse(data="<urn:subject> missing:predicate <urn:object> .", format="turtle")
+        Graph().parse(
+            data="<urn:subject> missing:predicate <urn:object> .", format="turtle"
+        )
 
     assert raised.value.msg == 'Prefix "missing:" not bound'
+
 
 test_data = """
 #  Definitions of terms describing the n3 model
